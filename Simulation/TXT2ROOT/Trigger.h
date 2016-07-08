4e3ac1a6a513f4ca111d72cf469e57e71c3e9b3b
Alessio Sarti <alessio.sarti@lnf.infn.it>
2016-07-08 17:00:18 +0200

    Fixed trigger dependencies on non existent detectors + makefile for linux

--- Simulation/TXT2ROOT/Trigger.h
+++ Simulation/TXT2ROOT/Trigger.h
@@ -32,13 +32,6 @@ typedef struct {
 	Int_t EventNumber;
 	Bool_t TopLevel;//Top level trigger
 	Bool_t Magarita;//start counter
-	Bool_t SmallEndCap;//small end-cap
-	Bool_t BigEndCap;//big end-cap
-	Bool_t Barrel;//barrel
-	Bool_t Onion;//onion veto
-	Bool_t TofWallFrontSingle;//TOF-Wall front wall slat hit
-	Bool_t TofWallFrontMulti;//TOF-Wall front wall multiple slats hit
-
 	Bool_t Wanted;//Is this an event we want
 
 } TRIGGER_STRUCT;
@@ -76,13 +69,6 @@ private:
 	Bool_t fTR_Flag;//trigger method already called this event?
 	Bool_t fTR_TopLevel;//Top level trigger
 	Bool_t fTR_ST;//start counter
-	Bool_t fTR_SE;//small end-cap
-	Bool_t fTR_BE;//big end-cap
-	Bool_t fTR_BR;//barrel
-	Bool_t fTR_ON;//onion veto
-	Bool_t fTR_TWF;//TOF-Wall front wall slat hit
-	Bool_t fTR_TWFMulti;//TOF-Wall front wall multiple slats hit
-
 	Bool_t fIsWanted;//is a wanted event
 
 	//evaluation variables
@@ -95,23 +81,6 @@ private:
 	//Trigger calibration parameters
 	//Energy deposition thresholds
 	constexpr static Double_t kTR_ST_Thres = 0.0; //UNITS:GeV MAGARITA
-	constexpr static Double_t kTR_SE_Thres = 0.002; //UNITS:GeV KENTROS small end-cap
-	constexpr static Double_t kTR_BE_Thres = 0.002; //UNITS:GeV KENTROS big end-cap
-	constexpr static Double_t kTR_BR_Thres = 0.002; //UNITS:GeV KENTROS barrel
-	constexpr static Double_t kTR_TW_Thres = 0.002; //UNITS:GeV TOF-Wall
-	constexpr static Double_t kTR_ON_Thres = 0.010; //UNITS:GeV ONION
-//KENTROS: Bruno and Piernicola:
-//	However, in this moment a reasonable threshold value for the deposited energy
-//	can be considered as a fraction of the energy deposited by a MIP in the
-//	scintillator (~ 7 MeV).
-//	So we consider that about 2-3 MeV can be considered an acceptable value for the
-//	threshold for the triggering signal.
-
-
-	constexpr static Int_t kTR_ON_UntilRingNumber = 3; //ONION number of rings in ONION used for triggering
-	//maximum is: TW_Onion_Nb
-	//3: is about 95% of primaries (12C@400 on C)
-	//4: is about 98% of primaries (12C@400 on C)
 
 	constexpr static Double_t kWantedEvent_Thres =10.0; //10.0;//UNITS:MeV/n
 
@@ -129,72 +98,6 @@ public:
 		}
 
 		return fTR_ST;
-	}
-
-	Bool_t IsSmallEndCap() const {
-		if (!fTR_Flag) {
-			cerr
-					<< "ERROR: Trigger::IsSmallEndCap(): Call Trigger::ComputeTrigger() before calling this method!"
-					<< endl;
-			exit(1);
-		}
-
-		return fTR_SE;
-	}
-
-	Bool_t IsBigEndCap() const {
-		if (!fTR_Flag) {
-			cerr
-					<< "ERROR: Trigger::IsBigEndCap(): Call Trigger::ComputeTrigger() before calling this method!"
-					<< endl;
-			exit(1);
-		}
-
-		return fTR_BE;
-	}
-
-	Bool_t IsBarrel() const {
-		if (!fTR_Flag) {
-			cerr
-					<< "ERROR: Trigger::IsBarrel(): Call Trigger::ComputeTrigger() before calling this method!"
-					<< endl;
-			exit(1);
-		}
-
-		return fTR_BR;
-	}
-
-	Bool_t IsTOFWallFSingle() const {
-		if (!fTR_Flag) {
-			cerr
-					<< "ERROR: Trigger::IsTOFWallFSingle(): Call Trigger::ComputeTrigger() before calling this method!"
-					<< endl;
-			exit(1);
-		}
-
-		return fTR_TWF;
-	}
-
-	Bool_t IsTOFWallFMulti() const {
-		if (!fTR_Flag) {
-			cerr
-					<< "ERROR: Trigger::IsTOFWallFMulti(): Call Trigger::ComputeTrigger() before calling this method!"
-					<< endl;
-			exit(1);
-		}
-
-		return fTR_TWFMulti;
-	}
-
-	Bool_t IsONION() const {
-		if (!fTR_Flag) {
-			cerr
-					<< "ERROR: Trigger::IsONION(): Call Trigger::ComputeTrigger() before calling this method!"
-					<< endl;
-			exit(1);
-		}
-
-		return fTR_ON;
 	}
 
 	Bool_t IsTopLevel() const {
@@ -249,13 +152,6 @@ public:
 			cout << endl;
 			cout << " Threshold settings:           " << endl;
 			cout << " MARARITA:    " << kTR_ST_Thres * 1000. << "MeV" << endl;
-			cout << " KENTROS(SE,BE,BR): " << kTR_SE_Thres * 1000. << ", "
-					<< kTR_BE_Thres * 1000. << ", " << kTR_BR_Thres * 1000.
-					<< "MeV" << endl;
-			cout << " TOF-Wall:    " << kTR_TW_Thres * 1000. << "MeV" << endl;
-			cout << " ONION:       " << kTR_ON_Thres * 1000.
-					<< "MeV, Number of rings: " << kTR_ON_UntilRingNumber
-					<< endl;
 			cout << " Wanted events kinetic energy threshold: "
 					<< kWantedEvent_Thres << "MeV/n" << endl;
 			cout << "=================================================="
