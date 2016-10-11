ff700508e93bb78307fdb760a2c9bc69a09a33d4
Alessio Sarti <alessio.sarti@lnf.infn.it>
2016-10-11 14:58:07 +0200

    linking with magnet

---
+++ Simulation/link_FOOT_mag.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cd ROUTINES
+$FLUPRO/flutil/fff  usrini.f
+$FLUPRO/flutil/fff  usrein.f
+$FLUPRO/flutil/fff  usreou.f
+$FLUPRO/flutil/fff  usrout.f
+$FLUPRO/flutil/fff  mgdraw.f
+$FLUPRO/flutil/fff  magfld.f
+$FLUPRO/flutil/fff  mgdraw_lib.f
+$FLUPRO/flutil/fff  UpdateCurrentParticle.f
+
+$FLUPRO/flutil/ldpm3qmd -m fluka usrini.o usrout.o usreou.o usrein.o  mgdraw.o magfld.o mgdraw_lib.o UpdateCurrentParticle.o -o fluka_FOOT_mag.exe
+
+rm *.o
+
+mv fluka_FOOT_mag.exe ../
+
+cd ../
+
+
+
+
