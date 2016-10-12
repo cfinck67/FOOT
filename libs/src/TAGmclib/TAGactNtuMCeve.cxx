a43c2414479e0a40f0cdc38d905847bf98aae30e
Serena Marta Valle <vallefoott3@uibo-foot-01.cr.cnaf.infn.it>
2016-10-12 09:56:42 +0200

    revised

--- libs/src/TAGmclib/TAGactNtuMCeve.cxx
+++ libs/src/TAGmclib/TAGactNtuMCeve.cxx
@@ -56,21 +56,29 @@ Bool_t TAGactNtuMCeve::Action() {
 
   for (Int_t i = 0; i < fpEvtStr->trn; i++) {
     Double_t i_mass = fpEvtStr->trmass[i];
-    Int_t i_id = fpEvtStr->trid[i];
+    Int_t i_id = fpEvtStr->trid[i];//idpa
     //    Int_t i_type = fpEvtStr->trtype[i];
-    Int_t i_type = -999;
-    Int_t i_mid = fpEvtStr->trpaid[i];
+    Int_t i_type = -999;//what is it?jpa?
+    Int_t i_mid = fpEvtStr->trpaid[i];//jpa
     Int_t i_chg = fpEvtStr->trcha[i];
     Int_t i_reg = fpEvtStr->trreg[i];
     Int_t i_bar = fpEvtStr->trbar[i];
     Int_t i_dead = fpEvtStr->trdead[i];
+    Int_t i_moth = i_mid-1;
+    Double_t i_time = fpEvtStr->trtime[i];
+    Double_t i_tof = fpEvtStr->trtof[i];
+    Double_t i_trlen = fpEvtStr->trtrlen[i];
 
     TVector3 ipos = TVector3(fpEvtStr->trix[i],fpEvtStr->triy[i],fpEvtStr->triz[i]);
     TVector3 fpos = TVector3(fpEvtStr->trfx[i],fpEvtStr->trfy[i],fpEvtStr->trfz[i]);
     TVector3 ip = TVector3(fpEvtStr->tripx[i],fpEvtStr->tripy[i],fpEvtStr->tripz[i]);
+    TVector3 fp = TVector3(fpEvtStr->trfpx[i],fpEvtStr->trfpy[i],fpEvtStr->trfpz[i]);
+    //decide if propagate also final momentum
 
-    //    TVector3 mothp = TVector3(fpEvtStr->trpapx[i],fpEvtStr->trpapy[i],fpEvtStr->trpapz[i]);
-    TVector3 mothp = TVector3(-99,-99,-99);
+    TVector3 mothip = TVector3 (0,0,0);
+    if(i_moth>=0) mothip = TVector3(fpEvtStr->tripx[i_moth],fpEvtStr->tripy[i_moth],fpEvtStr->tripz[i_moth]);
+    TVector3 mothfp = TVector3 (0,0,0);
+    if(i_moth>=0) mothfp = TVector3(fpEvtStr->trfpx[i_moth],fpEvtStr->trfpy[i_moth],fpEvtStr->trfpz[i_moth]);
 
     //    Int_t i_pileup = fpEvtStr->trpileup[i]; // VM added 17/11/13 for pileup
     Int_t i_pileup = 0; // VM added 17/11/13 for pileup
@@ -79,7 +87,7 @@ Bool_t TAGactNtuMCeve::Action() {
 
     // VM changed 17/11/13
     new((*(p_nturaw->h))[i]) 
-      TAGntuMCeveHit(i_id,i_chg,i_type,i_reg,i_bar,i_dead,i_mass,ipos,fpos,ip,i_mid,mothp,i_pileup);
+      TAGntuMCeveHit(i_id,i_chg,i_type,i_reg,i_bar,i_dead,i_mass,i_moth,i_time,i_tof,i_trlen,ipos,fpos,ip,fp,mothip,mothfp,i_pileup);
     
   }
 
