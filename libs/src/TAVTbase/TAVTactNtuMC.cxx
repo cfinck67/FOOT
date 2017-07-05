78cd16835f8c31e23217cf73e3a04619a08ad1f0
Christian Finck <cfinck@iphc.cnrs.fr>
2017-07-05 14:48:52 +0200

    Re-indent

--- libs/src/TAVTbase/TAVTactNtuMC.cxx
+++ libs/src/TAVTbase/TAVTactNtuMC.cxx
@@ -1,8 +1,8 @@
 /*!
-  \file
-  \version $Id: TAVTactNtuMC.cxx,v 1.9 2003/06/22 10:35:48 mueller Exp $
-  \brief   Implementation of TAVTactNtuMC.
-*/
+ \file
+ \version $Id: TAVTactNtuMC.cxx,v 1.9 2003/06/22 10:35:48 mueller Exp $
+ \brief   Implementation of TAVTactNtuMC.
+ */
 
 #include <map>
 
@@ -21,9 +21,9 @@
 #include "GlobalPar.hxx"
 
 /*!
-  \class TAVTactNtuMC"
-  \brief NTuplizer for vertex raw hits. **
-*/
+ \class TAVTactNtuMC"
+ \brief NTuplizer for vertex raw hits. **
+ */
 
 ClassImp(TAVTactNtuMC);
 
@@ -31,28 +31,28 @@ ClassImp(TAVTactNtuMC);
 //! Default constructor.
 
 TAVTactNtuMC::TAVTactNtuMC(const char* name,
-               TAGdataDsc* pNtuRaw, 
-               TAGparaDsc* pGeoMap,
-               TAGparaDsc* pParMap,
-               EVENT_STRUCT* evStr)
-  : TAGaction(name, "TAVTactNtuMC - NTuplize Vertex MC data"),
-    fpNtuRaw(pNtuRaw),
-    fpGeoMap(pGeoMap),
-    fpParMap(pParMap),
-    fpEvtStr(evStr),
-    fDigitizer(new TAVTdigitizer(pGeoMap)),
-    fDebugLevel(0)
+                           TAGdataDsc* pNtuRaw,
+                           TAGparaDsc* pGeoMap,
+                           TAGparaDsc* pParMap,
+                           EVENT_STRUCT* evStr)
+: TAGaction(name, "TAVTactNtuMC - NTuplize Vertex MC data"),
+fpNtuRaw(pNtuRaw),
+fpGeoMap(pGeoMap),
+fpParMap(pParMap),
+fpEvtStr(evStr),
+fDigitizer(new TAVTdigitizer(pGeoMap)),
+fDebugLevel(0)
 {
-  AddDataOut(pNtuRaw, "TAVTntuRaw");
-  AddPara(pGeoMap, "TAVTparGeo");
-  AddPara(pParMap, "TAVTparMap");
+   AddDataOut(pNtuRaw, "TAVTntuRaw");
+   AddPara(pGeoMap, "TAVTparGeo");
+   AddPara(pParMap, "TAVTparMap");
 }
 
 //------------------------------------------+-----------------------------------
 //! Destructor.
 TAVTactNtuMC::~TAVTactNtuMC()
 {
-
+   
 }
 
 //------------------------------------------+-----------------------------------
@@ -62,36 +62,36 @@ void TAVTactNtuMC::CreateHistogram()
    DeleteHistogram();
    
    TAVTparGeo* pGeoMap  = (TAVTparGeo*) fpGeoMap->Object();
-
+   
    for (Int_t i = 0; i < pGeoMap->GetSensorsN(); ++i) {
       fpHisPixel[i] = new TH1F(Form("vtMcPixel%d", i+1), Form("Vertex - MC # pixels per clusters for sensor %d", i+1), 100, 0., 100.);
       AddHistogram(fpHisPixel[i]);
    }
-
+   
    
    for (Int_t i = 0; i < pGeoMap->GetSensorsN(); ++i) {
-     if (TAVTparConf::IsMapHistOn()) {
-       fpHisPixelMap[i]  = new TH2F(Form("vtMcPixelMap%d", i+1) , Form("Vertex - pixel map for sensor %d", i+1),
-                                    pGeoMap->GetNPixelX(), 0, pGeoMap->GetNPixelX(),
-                                    pGeoMap->GetNPixelY(), 0, pGeoMap->GetNPixelY());
-       fpHisPixelMap[i]->SetStats(kFALSE);
-       AddHistogram(fpHisPixelMap[i]);
-     }
+      if (TAVTparConf::IsMapHistOn()) {
+         fpHisPixelMap[i]  = new TH2F(Form("vtMcPixelMap%d", i+1) , Form("Vertex - pixel map for sensor %d", i+1),
+                                      pGeoMap->GetNPixelX(), 0, pGeoMap->GetNPixelX(),
+                                      pGeoMap->GetNPixelY(), 0, pGeoMap->GetNPixelY());
+         fpHisPixelMap[i]->SetStats(kFALSE);
+         AddHistogram(fpHisPixelMap[i]);
+      }
    }
-  
+   
    for (Int_t i = 0; i < pGeoMap->GetSensorsN(); ++i) {
-     if (TAVTparConf::IsMapHistOn()) {
-       fpHisPosMap[i] =  new TH2F(Form("vtMcPosMap%d", i+1), Form("Vertex - position map for sensor %d", i+1),
-                                  pGeoMap->GetNPixelX(), -pGeoMap->GetPitchX()/2*pGeoMap->GetNPixelX(), pGeoMap->GetPitchX()/2*pGeoMap->GetNPixelX(),
-                                  pGeoMap->GetNPixelY(), -pGeoMap->GetPitchY()/2*pGeoMap->GetNPixelY(), pGeoMap->GetPitchY()/2*pGeoMap->GetNPixelY());
-       fpHisPosMap[i]->SetStats(kFALSE);
-       AddHistogram(fpHisPosMap[i]);
-     }
-    
+      if (TAVTparConf::IsMapHistOn()) {
+         fpHisPosMap[i] =  new TH2F(Form("vtMcPosMap%d", i+1), Form("Vertex - position map for sensor %d", i+1),
+                                    pGeoMap->GetNPixelX(), -pGeoMap->GetPitchX()/2*pGeoMap->GetNPixelX(), pGeoMap->GetPitchX()/2*pGeoMap->GetNPixelX(),
+                                    pGeoMap->GetNPixelY(), -pGeoMap->GetPitchY()/2*pGeoMap->GetNPixelY(), pGeoMap->GetPitchY()/2*pGeoMap->GetNPixelY());
+         fpHisPosMap[i]->SetStats(kFALSE);
+         AddHistogram(fpHisPosMap[i]);
+      }
+      
    }
-  
+   
    SetValidHistogram(kTRUE);
-  return;
+   return;
 }
 
 
@@ -99,58 +99,58 @@ void TAVTactNtuMC::CreateHistogram()
 //! Action.
 Bool_t TAVTactNtuMC::Action()
 {
-
-  TAVTntuRaw* pNtuRaw = (TAVTntuRaw*) fpNtuRaw->Object();
-  TAVTparGeo* pGeoMap = (TAVTparGeo*) fpGeoMap->Object();
-
-
-  TAGgeoTrafo *fGeoTrafo =  (TAGgeoTrafo*)gTAGroot->FindAction(TAGgeoTrafo::GetDefaultActName().Data());
-  if (fGeoTrafo == 0x0)
-    Error("SetGeoTrafoName","No GeoTrafo action called %s available", TAGgeoTrafo::GetDefaultActName().Data());
-
-  pNtuRaw->Clear();
-
+   
+   TAVTntuRaw* pNtuRaw = (TAVTntuRaw*) fpNtuRaw->Object();
+   TAVTparGeo* pGeoMap = (TAVTparGeo*) fpGeoMap->Object();
+   
+   
+   TAGgeoTrafo *fGeoTrafo =  (TAGgeoTrafo*)gTAGroot->FindAction(TAGgeoTrafo::GetDefaultActName().Data());
+   if (fGeoTrafo == 0x0)
+      Error("SetGeoTrafoName","No GeoTrafo action called %s available", TAGgeoTrafo::GetDefaultActName().Data());
+   
+   pNtuRaw->Clear();
+   
    Int_t nPixelX = pGeoMap->GetNPixelX();
    
-
-    if (fDebugLevel)     Info("Action()","Processing n :: %2d hits \n",fpEvtStr->VTXn);
-    if ( GlobalPar::GetPar()->Debug() > 0 )     cout<< endl << "VTXn   " << fpEvtStr->VTXn<< endl;
-
-
+   
+   if (fDebugLevel)     Info("Action()","Processing n :: %2d hits \n",fpEvtStr->VTXn);
+   if ( GlobalPar::GetPar()->Debug() > 0 )     cout<< endl << "VTXn   " << fpEvtStr->VTXn<< endl;
+   
+   
    for (Int_t i = 0; i < fpEvtStr->VTXn; i++) {
-    if ( GlobalPar::GetPar()->Debug() > 0 )     cout<< endl << "FLUKA id =   " << fpEvtStr->TRfx[i] << "  "<< fpEvtStr->TRfy[i] << "  "<< fpEvtStr->TRfz[i] << endl;
-
-
+      if ( GlobalPar::GetPar()->Debug() > 0 )     cout<< endl << "FLUKA id =   " << fpEvtStr->TRfx[i] << "  "<< fpEvtStr->TRfy[i] << "  "<< fpEvtStr->TRfz[i] << endl;
+      
+      
       // Generated particle ID
       Int_t genPartID = fpEvtStr->VTXid[i] - 1;
       
-    if ( GlobalPar::GetPar()->Debug() > 0 )     {
-        cout << "Part type: " << fpEvtStr->TRfid[genPartID] << " and charge: " << fpEvtStr->TRcha[genPartID] << endl;      
-        cout << "Generated Position: " << fpEvtStr->TRix[genPartID] <<" "<<fpEvtStr->TRiy[genPartID]<<" "<<fpEvtStr->TRiz[genPartID] << endl;
-        cout << "Generated Momentum: " << fpEvtStr->TRipx[genPartID] <<" "<<fpEvtStr->TRipy[genPartID]<<" "<<fpEvtStr->TRipz[genPartID] << endl;
-    }
-
+      if ( GlobalPar::GetPar()->Debug() > 0 )     {
+         cout << "Part type: " << fpEvtStr->TRfid[genPartID] << " and charge: " << fpEvtStr->TRcha[genPartID] << endl;
+         cout << "Generated Position: " << fpEvtStr->TRix[genPartID] <<" "<<fpEvtStr->TRiy[genPartID]<<" "<<fpEvtStr->TRiz[genPartID] << endl;
+         cout << "Generated Momentum: " << fpEvtStr->TRipx[genPartID] <<" "<<fpEvtStr->TRipy[genPartID]<<" "<<fpEvtStr->TRipz[genPartID] << endl;
+      }
+      
       Int_t layer = fpEvtStr->VTXilay[i] ;
-     
+      
       Int_t sensorId = layer;
-
-
-     // set geometry // why ???
-   //  pixel->SetVtxGeo(pGeoMap);
-    
-   
+      
+      
+      // set geometry // why ???
+      //  pixel->SetVtxGeo(pGeoMap);
+      
+      
       Double_t eloss = fpEvtStr->VTXde[i];
       Double_t x     = (fpEvtStr->VTXxin[i]+fpEvtStr->VTXxout[i])/2;
       Double_t y     = (fpEvtStr->VTXyin[i]+fpEvtStr->VTXyout[i])/2;
       
-      fDigitizer->Process(eloss, x, y);
+      if (!fDigitizer->Process(eloss, x, y)) continue;
       std::map<int, int> map = fDigitizer->GetMap();
       
       // fill pixels from map
       std::map<int,int>::iterator it;
       
-       if ( GlobalPar::GetPar()->Debug() > 0 )
-          printf("x %.1f y %.1f\n", x, y);
+      if ( GlobalPar::GetPar()->Debug() > 0 )
+         printf("x %.1f y %.1f\n", x, y);
       
       for (it = map.begin(); it != map.end(); ++it) {
          if (map[it->first] == 1) {
@@ -174,10 +174,10 @@ Bool_t TAVTactNtuMC::Action()
             }
          }
       }
-
+      
       if (ValidHistogram()) {
          Int_t pixelsN = fDigitizer->GetPixelsN();
-        fpHisPixel[layer]->Fill(pixelsN);
+         fpHisPixel[layer]->Fill(pixelsN);
       }
    }
    
@@ -190,9 +190,9 @@ void TAVTactNtuMC::SetMCinfo(TAVTntuHitMC* pixel, Int_t hitId)
 {
    
    TAVTparGeo* pGeoMap = (TAVTparGeo*) fpGeoMap->Object();
-
+   
    Int_t mcID(-1000);
-
+   
    // Generated particle ID
    Int_t genPartID = fpEvtStr->VTXid[hitId] - 1;
    
@@ -232,9 +232,9 @@ void TAVTactNtuMC::SetMCinfo(TAVTntuHitMC* pixel, Int_t hitId)
    
    // store generated particle info
    pixel->SetGeneratedParticleInfo( genPartID, fpEvtStr->TRfid[genPartID], fpEvtStr->TRcha[genPartID],
-                                    fpEvtStr->TRbar[genPartID], fpEvtStr->TRmass[genPartID],
-                                    TVector3(fpEvtStr->TRix[genPartID], fpEvtStr->TRiy[genPartID], fpEvtStr->TRiz[genPartID]),
-                                    TVector3(fpEvtStr->TRipx[genPartID], fpEvtStr->TRipy[genPartID], fpEvtStr->TRipz[genPartID]) );
+                                   fpEvtStr->TRbar[genPartID], fpEvtStr->TRmass[genPartID],
+                                   TVector3(fpEvtStr->TRix[genPartID], fpEvtStr->TRiy[genPartID], fpEvtStr->TRiz[genPartID]),
+                                   TVector3(fpEvtStr->TRipx[genPartID], fpEvtStr->TRipy[genPartID], fpEvtStr->TRipz[genPartID]) );
    
 }
 
