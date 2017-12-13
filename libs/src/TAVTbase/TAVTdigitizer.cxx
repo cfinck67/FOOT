0ed904a84240579e327ad2e21fa5aba0328c4ba2
Christian Finck <cfinck@iphc.cnrs.fr>
2017-12-13 10:09:07 +0100

    Add base class for digitizer

--- libs/src/TAVTbase/TAVTdigitizer.cxx
+++ libs/src/TAVTbase/TAVTdigitizer.cxx
@@ -13,7 +13,6 @@
 
 #include "TAVTparGeo.hxx"
 #include "TAVTdigitizer.hxx"
-#include "TAVTdigitizer.hxx"
 
 /*!
  \class TAVTdigitizer 
@@ -22,9 +21,6 @@
 
 ClassImp(TAVTdigitizer);
 
-
-Double_t TAVTdigitizer::fgkGeV2keV = 1e6;
-Double_t TAVTdigitizer::fgkCm2Mu   = 10000;
 Int_t    TAVTdigitizer::fgkMaxTurn = 8;
 
 //00
@@ -51,21 +47,9 @@ Float_t TAVTdigitizer::fgkAngD05[] = {180.0,  11.3,  91.0,  18.5,  11.3,  11.3, 
 //------------------------------------------+-----------------------------------
 //! Default constructor.
 TAVTdigitizer::TAVTdigitizer(TAGparaDsc* parGeo)
-: TObject(),
-  fpParGeo(parGeo),
-  fPixelsN(-1),
-  fRsPar(1.6),
-  fRsParErr(0.2),
-  fThresPar(885),
-  fThresParErr(250),
-  fDebugLevel(0)
+: TAVTbaseDigitizer(parGeo)
 {
-   SetFunctions();
-   TAVTparGeo* pGeoMap  = (TAVTparGeo*) fpParGeo->Object();
-   fPitchX   = pGeoMap->GetPitchX();
-   fPixelsNx = pGeoMap->GetNPixelX();
-   fPitchY   = pGeoMap->GetPitchY();
-   fPixelsNy = pGeoMap->GetNPixelY();
+
 }
 
 //------------------------------------------+-----------------------------------
@@ -75,36 +59,8 @@ TAVTdigitizer::~TAVTdigitizer()
 
 }
 
-//------------------------------------------+-----------------------------------
-//! fill pixel signal
-Bool_t TAVTdigitizer::Process( Double_t edep, Double_t x0, Double_t y0)
-{
-   x0 *= fgkCm2Mu;
-   y0 *= fgkCm2Mu;
-   
-   Double_t deltaE = edep*fgkGeV2keV;
-   Double_t  smear = 0;
-   
-  // smear = gRandom->Gaus(0, fRsParErr);
-   fFuncClusterSize->SetParameter(0, fRsPar+smear);
-
- //  smear = gRandom->Gaus(0, fThresParErr);
-   fFuncClusterSize->SetParameter(1, fThresPar+smear);
-   
-   fPixelsN = TMath::Nint(fFuncClusterSize->Eval(deltaE));
-   
-   if (fPixelsN <= 0) fPixelsN = 1;
-   
-   if (fDebugLevel) {
-      printf("\nnext hit:\n");
-      printf("eloss %6.1f pixels %d\n", deltaE, fPixelsN);
-   }
-   
-   return MakeCluster(x0, y0);
-}
-
 // --------------------------------------------------------------------------------------
-Bool_t TAVTdigitizer::MakeCluster(Double_t x0, Double_t y0)
+Bool_t TAVTdigitizer::MakeCluster(Double_t x0, Double_t y0, Double_t /*zin*/, Double_t /*zout*/)
 {
    fMap.clear();
    fMapLast.clear();
@@ -178,7 +134,7 @@ Bool_t TAVTdigitizer::MakeCluster(Double_t x0, Double_t y0)
    
    
    Int_t rpixels = fPixelsN;
-   Int_t lastTurn = GetLastShell(fShel);
+   Int_t lastTurn = GetLastShell(fShel, fgkMaxTurn);
    Int_t npixels  = fShel[lastTurn];
    
    rpixels  = npixels - rpixels;
@@ -246,23 +202,6 @@ Bool_t TAVTdigitizer::MakeCluster(Double_t x0, Double_t y0)
    return true;
 }
 
-// --------------------------------------------------------------------------------------
-void  TAVTdigitizer::SetFunctions()
-{
-   // compute cluster size for a given Edep, x and y
-   fFuncClusterSize = new TF1("ClusterSize", this, &TAVTdigitizer::FuncClusterSize, 0, 2000, 2, "TAVTdigitizer", "FuncClusterSize");
-}
-
-// --------------------------------------------------------------------------------------
-Double_t TAVTdigitizer::FuncClusterSize(Double_t* x, Double_t* par)
-{
-   Float_t xx = x[0];
-   
-   Float_t f = 2*TMath::Pi()*par[0]*TMath::Log(xx/(2*TMath::Pi()*3.6e-3*par[1]));
-   
-   return f;
-}
-
 
 //_____________________________________________________________________________
 Bool_t TAVTdigitizer::SetRegion(Int_t regX, Int_t regY)
@@ -295,121 +234,6 @@ Bool_t TAVTdigitizer::SetRegion(Int_t regX, Int_t regY)
       return false;
    
    return true;
-}
-
-//_____________________________________________________________________________
-Int_t TAVTdigitizer::GetColumn(Float_t x) const
-{
-   Float_t xmin = -fPixelsNx*fPitchX/2.;
-   
-   if (x < xmin || x > -xmin) {
-      if (fDebugLevel)
-         Warning("GetColumn()", "Value of X: %f out of range +/- %f\n", x, xmin);
-      return -1;
-   }
-   
-   Int_t col = floor((x-xmin)/fPitchX);
-   return col;
-}
-
-//_____________________________________________________________________________
-Int_t TAVTdigitizer::GetLine(Float_t y) const
-{
-   // equivalent to  floor((-y-ymin)/fPitchV)-1
-   Float_t ymin = -fPixelsNy*fPitchY/2.;
-   
-   if (y < ymin || y > -ymin) {
-      if (fDebugLevel)
-         Warning("GetLine()", "Value of Y: %f out of range +/- %f\n", y, ymin);
-      return -1;
-   }
-   
-   Int_t line = floor((y-ymin)/fPitchY);
-   return fPixelsNy - line - 1;
-}
-
-//-----------------------------------------+---------------------------------
-Int_t TAVTdigitizer::GetIndex(Int_t line, Int_t column) const
-{
-   return line*fPixelsNx + column;
-}
-
-//_____________________________________________________________________________
-Float_t TAVTdigitizer::GetColRemainder(Float_t x) const
-{
-   Float_t xmin = -fPixelsNx*fPitchX/2.;
-   
-   if (x < xmin || x > -xmin) {
-      if (fDebugLevel)
-         Warning("GetColumn()", "Value of X: %f out of range +/- %f\n", x, xmin);
-      return -1;
-   }
-   
-   Float_t col = (x-xmin)/fPitchX - floor((x-xmin)/fPitchX);
-   return col;
-}
-
-//_____________________________________________________________________________
-Float_t TAVTdigitizer::GetLineRemainder(Float_t y) const
-{
-   // equivalent to  floor((-y-ymin)/fPitchV)-1
-   Float_t ymin = -fPixelsNy*fPitchY/2.;
-   
-   if (y < ymin || y > -ymin) {
-      if (fDebugLevel)
-         Warning("GetLine()", "Value of Y: %f out of range +/- %f\n", y, ymin);
-      return -1;
-   }
-   
-   Float_t line = 1 -((y-ymin)/fPitchY - floor((y-ymin)/fPitchY));
-   return line;
-}
-
-
-//_____________________________________________________________________________
-Int_t TAVTdigitizer::GetColRegion(Float_t x) const
-{
-   Float_t rem = GetColRemainder(x);
-   
-   if (rem < 0.33) // hit a corner
-      return 0;
-   else if (rem > 0.33 && rem < 0.66)
-      return 5; // hit the middle of the pixel
-   else if (rem > 0.66)
-      return 1; // hit the upper cormer
-   else
-      return -99;
-}
-
-//_____________________________________________________________________________
-Int_t TAVTdigitizer::GetLineRegion(Float_t y) const
-{
-   Float_t rem = GetLineRemainder(y);
-   
-   if (rem < 0.33)
-      return 0;
-   else if (rem > 0.33 && rem < 0.66)
-      return 5;
-   else if (rem > 0.66)
-      return 1;
-   else
-      return -99;
-}
-
-//_____________________________________________________________________________
-Int_t TAVTdigitizer::GetLastShell(Int_t* shell) const
-{
-   Int_t lastTurn = 0;
-   
-   while (fPixelsN > shell[lastTurn]) {
-      lastTurn++;
-      if (lastTurn >= fgkMaxTurn) {
-         lastTurn = fgkMaxTurn;
-         break;
-      }
-   }
-
-   return lastTurn;
 }
 
 //_____________________________________________________________________________
--- libs/src/TAVTbase/TAVTdigitizer.hxx
+++ libs/src/TAVTbase/TAVTdigitizer.hxx
@@ -16,90 +16,33 @@
 #include "TList.h"
 
 #include "TAGparaDsc.hxx"
+#include "TAVTbaseDigitizer.hxx"
 
 
 class TF1;
-class TAVTdigitizer : public TObject {
+class TAVTdigitizer : public TAVTbaseDigitizer {
    
 public:
    TAVTdigitizer(TAGparaDsc* parGeo);
    virtual ~TAVTdigitizer();
    
-   Bool_t       Process(Double_t edep, Double_t x0, Double_t y0);
-   Double_t     FuncClusterSize(Double_t* x, Double_t* par);
-   
-   Int_t        GetPixelsN() const           { return fPixelsN;    }
-   
-   void         SetRsPar(Double_t par)       { fRsPar = par;       }
-   void         SetRsParErr(Double_t par)    { fRsParErr = par;    }
-   void         SetThresPar(Double_t par)    { fThresPar = par;    }
-   void         SetThresParErr(Double_t par) { fThresParErr = par; }
-   
-   
-   Float_t      GetNPixelX() const           { return fPixelsNx;   }
-   Float_t      GetNPixelY() const           { return fPixelsNy;   }
-   
-   Float_t      GetPitchX()  const           { return fPitchX;     }
-   Float_t      GetPitchY()  const           { return fPitchX;     }
-
-   //! Get Map
-   std::map<int, int>  GetMap() const        { return fMap;        }
-   
-   //! Get column/line from x/y position
-   Int_t        GetColumn(Float_t x) const;
-   Int_t        GetLine(Float_t y)   const;
-   Int_t        GetIndex(Int_t line, Int_t column) const;
-   
-   //! Get remainder of column/line
-   Float_t      GetColRemainder(Float_t x)  const;
-   Float_t      GetLineRemainder(Float_t y) const;
-   
-   //! Get pixel region of column/line
-   Int_t        GetColRegion(Float_t x)  const;
-   Int_t        GetLineRegion(Float_t y) const;
-   
-public:
-   static Double_t GeV2keV()                 { return fgkGeV2keV;  }
-   static Double_t Cm2Mu()                   { return fgkCm2Mu;    }
-   
-   static Int_t    GetRandom (Int_t i)       { return std::rand() % i;}
+   Bool_t MakeCluster(Double_t x0, Double_t y0, Double_t zin = 0, Double_t zout = 0);
 
 private:
    TAGparaDsc* fpParGeo;
-   TF1*        fFuncClusterSize; // cluster size function
    
-   std::map<int, int> fMap;      // map of found pixels
    std::map<int, int> fMapLast;  // map of found pixels of the outer shell
    
-   Int_t       fPixelsN;         // number of pixels for a given eloss
-   Double_t    fRsPar;           // parameter r_s for the cluster size function
-   Double_t    fRsParErr;        // parameter r_s for the cluster size function
-   Double_t    fThresPar;        // parameter threshold for cluster size function
-   Double_t    fThresParErr;     // parameter threshold for cluster size function
-
-   Int_t       fPixelsNx;        // number of pixels in X (colummn)
-   Int_t       fPixelsNy;        // number of pixels in Y (line)
-   
-   Float_t     fPitchX;          // pitch in X
-   Float_t     fPitchY;          // pitch in Y
-   
-   Int_t*      fShel;
    Float_t*    fAngD;
    Float_t*    fFacX;
    Float_t*    fFacY;
    
-   Int_t       fDebugLevel;      // debug
    
 private:
-   void        SetFunctions();
-   Bool_t      MakeCluster(Double_t x0, Double_t y0);
-   Int_t       GetLastShell(Int_t* shell) const;
    void        RemovePixels(Int_t rpixels); // remove number of pixels from last shell randomly
    Bool_t      SetRegion(Int_t regX, Int_t regY);
    
-private:   
-   static Double_t fgkGeV2keV;
-   static Double_t fgkCm2Mu;
+private:
    
    //00
    static Int_t   fgkShel00[];
