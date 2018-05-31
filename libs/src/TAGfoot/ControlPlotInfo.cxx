96b3d77440756b848255b0a265622c9b2e045347
mengarelli <mengarelli@bo.infn.it>
2018-05-31 15:02:53 +0200

    Adding Multitracking and monitoring plots

--- libs/src/TAGfoot/ControlPlotInfo.cxx
+++ libs/src/TAGfoot/ControlPlotInfo.cxx
@@ -1,9 +1,7 @@
 
 #include "ControlPlotInfo.hxx"
 
-
-
-void  ControlPlotInfo::PrintHisto( TCanvas* mirror ) {
+void  ControlPlotInfo::PrintHisto( TCanvas* mirror) {
 	// check if parameter from file is enabled		-> 		otherwise guess them
 
 	// nBin
@@ -14,23 +12,69 @@ void  ControlPlotInfo::PrintHisto( TCanvas* mirror ) {
 
 	// range
 	if ( GlobalPar::GetPar()->GetNBinHisto( m_name ) != -666 ) {
-		m_minBin = GlobalPar::GetPar()->GetNBinHisto( m_name ); 
-		m_maxBin = GlobalPar::GetPar()->GetUpBinHisto( m_name ); 
+		m_minBin_x = GlobalPar::GetPar()->GetNBinHisto( m_name ); 
+		m_maxBin_x = GlobalPar::GetPar()->GetUpBinHisto( m_name ); 
+
+		m_minBin_y = GlobalPar::GetPar()->GetNBinHisto( m_name ); 
+		m_maxBin_y = GlobalPar::GetPar()->GetUpBinHisto( m_name ); 
+
+
 	}
 	else {
+
+	  
+	  if(!isHisto2D){
 		// automatic calculation of the range if not defined in the parameters
 		if ( isDoub ) {
-			double min =  *min_element( m_variablesDoub.begin(), m_variablesDoub.end() );
-			double max =  *max_element( m_variablesDoub.begin(), m_variablesDoub.end() );
+		  double min_x =  *min_element( m_variablesDoub_x.begin(), m_variablesDoub_x.end() );
+		  double max_x =  *max_element( m_variablesDoub_x.begin(), m_variablesDoub_x.end() );
+	      
+		  double length_x = max_x - min_x;
+		  m_minBin_x = min_x - length_x*0.1;
+		  m_maxBin_x = max_x + length_x*0.1;
+	      
 
-			double length = max - min;
-			m_minBin = min - length*0.1;
-			m_maxBin = max + length*0.1;
 		}
 		if ( isInt ) {
-			m_minBin = *min_element( m_variablesInt.begin(), m_variablesInt.end() ) - 1;
-			m_maxBin = *max_element( m_variablesInt.begin(), m_variablesInt.end() ) + 1;
+			m_minBin_x = *min_element( m_variablesInt_x.begin(), m_variablesInt_x.end() ) - 1;
+			m_maxBin_x = *max_element( m_variablesInt_x.begin(), m_variablesInt_x.end() ) + 1;
 		}
+	  }
+	  else if(isHisto2D){
+	    
+	    	// automatic calculation of the range if not defined in the parameters
+	    if ( isDoub ) {
+	      double min_x =  *min_element( m_variablesDoub_x.begin(), m_variablesDoub_x.end() );
+	      double max_x =  *max_element( m_variablesDoub_x.begin(), m_variablesDoub_x.end() );
+
+	      double min_y =  *min_element( m_variablesDoub_y.begin(), m_variablesDoub_y.end() );
+	      double max_y =  *max_element( m_variablesDoub_y.begin(), m_variablesDoub_y.end() );
+
+	      double length_x = max_x - min_x;
+	      m_minBin_x = min_x - length_x*0.1;
+	      m_maxBin_x = max_x + length_x*0.1;
+	      
+	      double length_y = max_y - min_y;
+	      m_minBin_y = min_y - length_y*0.1;
+	      m_maxBin_y = max_y + length_y*0.1;
+	      
+	      
+	    }
+	    if ( isInt ) {
+	      m_minBin_x = *min_element( m_variablesInt_x.begin(), m_variablesInt_x.end() ) - 1;
+	      m_maxBin_x = *max_element( m_variablesInt_x.begin(), m_variablesInt_x.end() ) + 1;
+
+	      m_minBin_y = *min_element( m_variablesInt_y.begin(), m_variablesInt_y.end() ) - 1;
+	      m_maxBin_y = *max_element( m_variablesInt_y.begin(), m_variablesInt_y.end() ) + 1;
+	      
+
+	    }
+	    
+	    
+	    
+
+	  }
+	    
 	}
 
 	// saving directory
@@ -44,31 +88,61 @@ void  ControlPlotInfo::PrintHisto( TCanvas* mirror ) {
 		m_titleY = GlobalPar::GetPar()->GetYTitlesHisto( m_name ); 
 
 
-	// init histo
-	TH1F* histo = new TH1F( m_name.c_str(), m_title.c_str(), m_nBin, m_minBin, m_maxBin );
 	
-	// fill histo 
-	if (isDoub) {
-		for (unsigned int i=0; i<m_variablesDoub.size(); i++) 
-			histo->Fill( m_variablesDoub.at(i) );
+	if(!isHisto2D){
+	  // init histo 1D
+	  histo1D = new TH1F( m_name.c_str(), m_title.c_str(), m_nBin, m_minBin_x, m_maxBin_x );
+	  
+	  // fill histo 1D
+	  if (isDoub) {
+	    for (unsigned int i=0; i<m_variablesDoub_x.size(); i++) 
+	      histo1D->Fill( m_variablesDoub_x.at(i) );
+	  }
+	  if (isInt) {
+	    for (unsigned int i=0; i<m_variablesInt_x.size(); i++) 
+	      histo1D->Fill( m_variablesInt_x.at(i) );
+	  }
+	  
+	  if ( m_titleX != "" )		histo1D->GetXaxis()->SetTitle( m_titleX.c_str() );
+	  if ( m_titleY != "" )		histo1D->GetYaxis()->SetTitle( m_titleY.c_str() );
+	  
 	}
-	if (isInt) {
-		for (unsigned int i=0; i<m_variablesInt.size(); i++) 
-			histo->Fill( m_variablesInt.at(i) );
+	else if(isHisto2D){
+	  // init histo 2D
+	  histo2D = new TH2F( m_name.c_str(), m_title.c_str(), m_nBin, m_minBin_x, m_maxBin_x, m_nBin, m_minBin_y, m_maxBin_y );
+	  
+	  // fill histo 2D
+	  if (isDoub) {
+	    for (unsigned int i=0; i<m_variablesDoub_x.size(); i++) 
+	      histo2D->Fill( m_variablesDoub_x.at(i),  m_variablesDoub_y.at(i));
+	    
+	  }
+	  if (isInt) {
+	    for (unsigned int i=0; i<m_variablesInt_x.size(); i++) 
+	      histo2D->Fill( m_variablesInt_x.at(i),m_variablesInt_y.at(i) );
+	  }
+	  
+	  if ( m_titleX != "" )		histo2D->GetXaxis()->SetTitle( m_titleX.c_str() );
+	  if ( m_titleY != "" )		histo2D->GetYaxis()->SetTitle( m_titleY.c_str() );
+	  
+
 	}
 
-	if ( m_titleX != "" )		histo->GetXaxis()->SetTitle( m_titleX.c_str() );
-	if ( m_titleY != "" )		histo->GetYaxis()->SetTitle( m_titleY.c_str() );
+	  
 
+	
+	
 
-	if (!GlobalPar::GetPar()->IsPrintOutputFile()) { 
+	m_printoutfile = GlobalPar::GetPar()->IsPrintOutputFile();
+
+	if (!m_printoutfile) { 
 
 		struct stat info;
 		if( stat( m_dir.c_str(), &info ) != 0 )		//cannot access
 		    system(("mkdir "+m_dir).c_str());
 
 		// save histo
-		histo->Draw();
+		//	histo->Draw();
 		mirror->SaveAs(  (m_dir + "/" + m_name + ".png").c_str() ); // very slow operation !!!!!!!!!!!!!!!!
 	}
 
