1aa428c31a140964b984374e017836d98ced9e50
Christian Finck <cfinck@iphc.cnrs.fr>
2018-07-20 15:36:21 +0200

    Remove map for disentangle hits, using MC id (as in FIRST), not compliant with TTree

--- libs/src/TAVTbase/TAVTntuRaw.cxx
+++ libs/src/TAVTbase/TAVTntuRaw.cxx
@@ -1,14 +1,18 @@
 /*!
-  \file
-  \version $Id: TAVTntuRaw.cxx,v 1.12 2003/06/09 18:41:17 mueller Exp $
-  \brief   Implementation of TAVTntuRaw.
-*/
+ \file
+ \version $Id: TAVTntuRaw.cxx,v 1.12 2003/06/09 18:41:17 mueller Exp $
+ \brief   Implementation of TAVTntuRaw.
+ */
 
+#include "TString.h"
+#include "TClonesArray.h"
+
+#include "TAVTntuRaw.hxx"
 
 /*!
-  \class TAVTntuRaw TAVTntuRaw.hxx "TAVTntuRaw.hxx"
-  \brief Container class for VTX ntu hit **
-*/
+ \class TAVTntuRaw TAVTntuRaw.hxx "TAVTntuRaw.hxx"
+ \brief Container class for VTX ntu hit **
+ */
 
 ////////////////////////////////////////////////////////////
 // Class Description of TAVTntuHit                        //
@@ -17,179 +21,46 @@
 ////////////////////////////////////////////////////////////
 
 
-#include "TString.h"
-#include "TClonesArray.h"
-
-#include "TAVTntuRaw.hxx"
-
+#include "TAGroot.hxx"
+#include "TAVTparGeo.hxx"
 #include "TAVTdatRaw.hxx"
-
+#include "TAVTdatRaw.hxx"
 
 
 //##############################################################################
 
 ClassImp(TAVTntuRaw);
 
-// TString TAVTntuRaw::fgkBranchName   = "vtrh.";
-
-
+TString TAVTntuRaw::fgkBranchName   = "vtrh.";
 
 //------------------------------------------+-----------------------------------
 //! Default constructor.
-TAVTntuRaw::TAVTntuRaw() 
+TAVTntuRaw::TAVTntuRaw()
 : TAGdata(),
-  m_listOfPixels(0x0)
+  fListOfPixels(0x0),
+  fpGeoMap(0x0)
 {
-    cout << "TAVTntuRaw::TAVTntuRaw()" << endl;
-    m_vtxGeo = (TAVTparGeo*) gTAGroot->FindParaDsc("vtGeo", "TAVTparGeo")->Object();
-    SetupClones();
+   fpGeoMap = (TAVTparGeo*) gTAGroot->FindParaDsc(TAVTparGeo::GetDefParaName(), "TAVTparGeo")->Object();
+   if (!fpGeoMap) {
+      Error("TAVTntuRaw()", "Para desciptor vtGeo does not exist");
+      exit(0);
+   }
+
+   SetupClones();
 }
-
-
 
 //------------------------------------------+-----------------------------------
 //! Destructor.
-TAVTntuRaw::~TAVTntuRaw() {
-    delete m_listOfPixels;
-    m_mcHitList.clear();
-    m_mcClusterList.clear();
-    m_noiseList.clear();
-    m_pileUpList.clear();
+TAVTntuRaw::~TAVTntuRaw()
+{
+   delete fListOfPixels;
 }
 
-
-
-
-
-//______________________________________________________________________________
-//  Deprecated, to be private
-TAVTntuHit* TAVTntuRaw::NewPixel(Int_t iSensor, Double_t value, Int_t aLine, Int_t aColumn){
-   if (iSensor >= 0  && iSensor < m_vtxGeo->GetNSensors()) {      
-      TClonesArray &pixelArray = *GetListOfPixels(iSensor);
-      TAVTntuHit* pixel = new(pixelArray[pixelArray.GetEntriesFast()]) TAVTntuHit(iSensor, value, aLine, aColumn, "deprecated");
-      return pixel;
-   } else {
-      Error("NewPixel()", "Wrong sensor number %d\n", iSensor);
-      return 0x0;
-   }
-}
-
-
-
-
-//______________________________________________________________________________
-//  standard 
-TAVTntuHit* TAVTntuRaw::NewPixel(Int_t iSensor, Double_t value, Int_t aLine, Int_t aColumn, string aorigin) {
-
-   if ( iSensor >= 0  && iSensor < m_vtxGeo->GetNSensors() ) {
-
-        // check on aorigin
-        TClonesArray &pixelArray = *GetListOfPixels(iSensor);
-        FillPixelList( iSensor, aorigin, pixelArray.GetEntriesFast() ); 
-        TAVTntuHit* pixel = new(pixelArray[pixelArray.GetEntriesFast()]) TAVTntuHit(iSensor, value, aLine, aColumn, aorigin);
-        return pixel;
-
-    } else {
-        Error("NewPixel()", "Wrong sensor number %d\n", iSensor);
-        return 0x0;
-    }
-}
-
-
-
-
-//______________________________________________________________________________
-// Add MC original hits
-TAVTntuHit* TAVTntuRaw::NewPixel(int iSensor, double value, int aLine, int aColumn, string aorigin, int amcID, int agenPartID) {
-
-    if ( aorigin != "mc_hit" ) {
-        cout << "ERROR >> TAVTntuRaw::NewPixel  -->  developer error, input command not allowed " << aorigin << endl, exit(0);
-    }
-
-    if ( GlobalPar::GetPar()->Debug() > 1 )  cout << "TAVTntuRaw::NewPixel  " << iSensor << "  " << agenPartID << endl;
-
-    TAVTntuHit* pixel = NewPixel( iSensor, value, aLine, aColumn, aorigin );
-
-    // pixel->SetOrigin( aorigin );
-    if ( aorigin == "mc_hit" ) {
-        pixel->SetMCid( amcID );
-        pixel->SetGenPartID( agenPartID );
-    }
-    return pixel;
-}
-
-
-
-//______________________________________________________________________________
-// Add MC original hits
-TAVTntuHit* TAVTntuRaw::NewPixel(int iSensor, double value, int aLine, int aColumn, string aorigin, TAVTntuHit* originatingHit ) {
-
-    if ( aorigin != "mc_cluster" )  {
-        cout << "ERROR >> TAVTntuRaw::NewPixel  -->  developer error, input command not allowed " << aorigin << endl, exit(0);
-    }
-
-    TAVTntuHit* pixel = NewPixel( iSensor, value, aLine, aColumn, aorigin );
-
-    if ( aorigin == "mc_cluster" ) {
-        pixel->SetOriginalMC_Hit( originatingHit );
-
-    }
-    
-    return pixel;
-}
-
-
-
-//______________________________________________________________________________
-//  
-TAVTntuHit* TAVTntuRaw::NewPixel( int iSensor, TAVTrawHit* rawPixel ) {
-
-   if ( iSensor >= 0  && iSensor < m_vtxGeo->GetNSensors()) {      
-   // if (iSensor >= 0  || iSensor < m_vtxGeo->GetNSensors()) {      
-      TClonesArray &pixelArray = *GetListOfPixels(iSensor);
-      TAVTntuHit* pixel = new(pixelArray[pixelArray.GetEntriesFast()]) TAVTntuHit(iSensor, rawPixel);
-      return pixel;
-   } else {
-      Error("NewPixel()", "Wrong sensor number %d\n", iSensor);
-      return 0x0;
-   }   
-}
-
-
-
-
-//______________________________________________________________________________
-void TAVTntuRaw::FillPixelList(int iSensor, string command, int id ) {
-
-    if ( command == "mc_hit" ) {
-        m_mcHitList[iSensor].push_back(id);
-    }
-    else if ( command == "mc_cluster" ) {
-        m_mcClusterList[iSensor].push_back(id);
-    }
-    else if ( command == "noise" ) {
-        m_noiseList[iSensor].push_back(id);
-    }
-    else if ( command == "pileup" ) {
-        m_pileUpList[iSensor].push_back(id);
-    }
-    else {
-        cout << "ERROR >> TAVTntuRaw::FillPixelList  -->  input command string not allowed" << endl;
-        exit(0);
-    }
-
-}
-
-
-
-
-// private!!!!  // all by default 
 //------------------------------------------+-----------------------------------
-//! return number of pixels for a given sensor.  
-Int_t TAVTntuRaw::GetPixelsN(Int_t iSensor) const {
-
-   // if (iSensor >= 0  || iSensor < TAVTparMap::GetSensorsN()) {
-   if (iSensor >= 0  && iSensor < m_vtxGeo->GetNSensors()) {
+//! return number of pixels for a given sensor.
+Int_t TAVTntuRaw::GetPixelsN(Int_t iSensor) const
+{
+   if (iSensor >= 0  || iSensor < fpGeoMap->GetNSensors()) {
       TClonesArray*list = GetListOfPixels(iSensor);
       return list->GetEntries();
    } else  {
@@ -198,210 +69,143 @@ Int_t TAVTntuRaw::GetPixelsN(Int_t iSensor) const {
    }
 }
 
-
-
-
 //------------------------------------------+-----------------------------------
-//! return number of pixels for a given sensor.
-int TAVTntuRaw::GetPixelsN( int iSensor, string command ) {
-
-    // if ( iSensor >= 0  || iSensor < m_vtxGeo->GetNSensors() ) {
-    if ( iSensor < 0 || iSensor >= m_vtxGeo->GetNSensors() ) {
-        cout << "ERROR >> TAVTntuRaw::GetPixelsN  -->  number of sensor required is wrong" << iSensor<< endl;
-        exit(0);
-    }
-
-
-    if ( command == "mc_hit" ) {
-        return m_mcHitList[iSensor].size();
-    }
-    else if ( command == "mc_cluster" ) {
-        return m_mcClusterList[iSensor].size();
-    }
-    else if ( command == "noise" ) {
-        return m_noiseList[iSensor].size();
-    }
-    else if ( command == "pileup" ) {
-        return m_pileUpList[iSensor].size();
-    }
-    else if ( command == "forPhys" ) {
-        return m_mcClusterList[iSensor].size() + m_pileUpList[iSensor].size() + m_noiseList[iSensor].size();
-    }
-    else if ( command == "all" ) {
-        TClonesArray*list = GetListOfPixels(iSensor);
-        return list->GetEntries();
-    }
-    else  {
-        cout << "ERROR >> TAVTntuRaw::GetPixelsN  -->  input command string not allowed" << endl;
-        exit(0);
-    }
-    return -1;
+TClonesArray* TAVTntuRaw::GetListOfPixels(Int_t iSensor)
+{
+   if (iSensor >= 0  || iSensor < fpGeoMap->GetNSensors()) {
+      TClonesArray* list = (TClonesArray*)fListOfPixels->At(iSensor);
+      return list;
+   } else {
+      Error("GetListOfPixels()", "Wrong sensor number %d\n", iSensor);
+      return 0x0;
+   }
 }
 
-
-
-
+//------------------------------------------+-----------------------------------
+TClonesArray* TAVTntuRaw::GetListOfPixels(Int_t iSensor) const
+{
+   if (iSensor >= 0  || iSensor < fpGeoMap->GetNSensors()) {
+      TClonesArray* list = (TClonesArray*)fListOfPixels->At(iSensor);
+      return list;
+   } else {
+      Error("GetListOfPixels()", "Wrong sensor number %d\n", iSensor);
+      return 0x0;
+   }
+}
 
 //------------------------------------------+-----------------------------------
 //! return a pixel for a given sensor
-TAVTntuHit* TAVTntuRaw::GetPixel( int iSensor, int iPixel, string command ) {
-
-    if (  iSensor < 0 || iSensor >= m_vtxGeo->GetNSensors() ) {
-        cout << "ERROR >> TAVTntuRaw::GetPixel  -->  number of sensor required is wrong" << iSensor << endl;
-        exit(0);
-    }
-    if ( iPixel < 0 || iPixel >= GetPixelsN(iSensor) ) {    // wrong check by Frank
-        cout << "ERROR >> TAVTntuRaw::GetPixel  -->  number of pixel "<<iPixel<<" required is wrong " << GetPixelsN(iSensor) << endl;
-        exit(0);
-    }
-
-    TClonesArray* list = GetListOfPixels( iSensor );
-    if ( command == "mc_hit" ) {
-        return (TAVTntuHit*)list->At( m_mcHitList[iSensor].at(iPixel) );
-    }
-    else if ( command == "mc_cluster" ) {
-        return (TAVTntuHit*)list->At( m_mcClusterList[iSensor].at(iPixel) );
-    }
-    else if ( command == "noise" ) {
-        return (TAVTntuHit*)list->At( m_noiseList[iSensor].at(iPixel) );
-    }
-    else if ( command == "pileup" ) {
-        return (TAVTntuHit*)list->At( m_pileUpList[iSensor].at(iPixel) );
-    }
-    else if ( command == "forPhys" ) {
-        if ( iPixel >= 0 && iPixel < m_mcHitList[iSensor].size() )
-            return (TAVTntuHit*)list->At( m_mcHitList[iSensor].at(iPixel) );
-
-        else if ( iPixel >= m_mcHitList[iSensor].size() && (iPixel < m_mcHitList[iSensor].size() + m_pileUpList[iSensor].size()) )
-            return (TAVTntuHit*)list->At( m_pileUpList[iSensor].at(iPixel - m_mcHitList[iSensor].size()) );
-
-        else if ( (iPixel >= m_mcHitList[iSensor].size() + m_pileUpList[iSensor].size()) && (iPixel < m_mcHitList[iSensor].size() + m_pileUpList[iSensor].size() + m_noiseList[iSensor].size()) )
-            return (TAVTntuHit*)list->At( m_noiseList[iSensor].at( iPixel - m_mcHitList[iSensor].size() - m_pileUpList[iSensor].size() ) );
-    }
-    else if ( command == "all" ) {
-        return (TAVTntuHit*)list->At( iPixel );
-    }
-    else {
-        cout << "ERROR >> TAVTntuRaw::GetPixel  -->  command "<<command<<" not allowed " << endl;
-        exit(0);
-    }
-    return NULL;  // never happens, but compiler doesn't complain
+TAVTntuHit* TAVTntuRaw::GetPixel(Int_t iSensor, Int_t iPixel)
+{
+   if (iPixel >=0 || iPixel < GetPixelsN(iSensor)) {
+      TClonesArray* list = GetListOfPixels(iSensor);
+      return (TAVTntuHit*)list->At(iPixel);
+   } else {
+      Error("GetPixel()", "Wrong sensor number %d\n", iSensor);
+      return 0x0;
+   }
 }
-
-
-
 
 //------------------------------------------+-----------------------------------
 //! return a pixel for a given sensor
-const TAVTntuHit* TAVTntuRaw::GetPixel(Int_t iSensor, Int_t iPixel, string command) const {
-   // if (iPixel >=0 || iPixel < GetPixelsN(iSensor)) {
-   //    TClonesArray* list = GetListOfPixels(iSensor);
-   //    return (TAVTntuHit*)list->At(iPixel);
-   // } else {
-   //    Error("GetPixel()", "Wrong sensor number %d\n", iSensor);
-   //    return 0x0;
-   // }  
-   return GetPixel( iSensor, iPixel, command );
-}
-
-
-
-
-
-//------------------------------------------+-----------------------------------
-TClonesArray* TAVTntuRaw::GetListOfPixels(int iSensor)  {
-
-   if (iSensor >= 0  && iSensor < m_vtxGeo->GetNSensors()) {
-	  TClonesArray* list = (TClonesArray*)m_listOfPixels->At(iSensor);
-	  return list;
+const TAVTntuHit* TAVTntuRaw::GetPixel(Int_t iSensor, Int_t iPixel) const
+{
+   if (iPixel >=0 || iPixel < GetPixelsN(iSensor)) {
+      TClonesArray* list = GetListOfPixels(iSensor);
+      return (TAVTntuHit*)list->At(iPixel);
    } else {
-	  Error("GetListOfPixels()", "Wrong sensor number %d\n", iSensor);
-	  return 0x0;
-   }   
+      Error("GetPixel()", "Wrong sensor number %d\n", iSensor);
+      return 0x0;
+   }
 }
 
-
-
-
-
 //------------------------------------------+-----------------------------------
-TClonesArray* TAVTntuRaw::GetListOfPixels(int iSensor) const {
-
-   if (iSensor >= 0  && iSensor < m_vtxGeo->GetNSensors()) {
-	  TClonesArray* list = (TClonesArray*)m_listOfPixels->At(iSensor);
-	  return list;
-   } else {
-	  Error("GetListOfPixels()", "Wrong sensor number %d\n", iSensor);
-	  return 0x0;
-   }   
-}
-
-
-
-
-
-//------------------------------------------+-----------------------------------
-//! Setup clones. Crate and initialise the list of pixels
+//! Setup clones.
 void TAVTntuRaw::SetupClones()
 {
-   if (m_listOfPixels) return;
-   m_listOfPixels = new TObjArray();
+   if (fListOfPixels) return;
+   fListOfPixels = new TObjArray();
    
-   for (Int_t i = 0; i < m_vtxGeo->GetNSensors(); ++i) {
-	  TClonesArray* arr = new TClonesArray("TAVTntuHit", 500);
-	  arr->SetOwner(true);
-	  m_listOfPixels->AddAt(arr, i);
+   for (Int_t i = 0; i < fpGeoMap->GetNSensors(); ++i) {
+      TClonesArray* arr = new TClonesArray("TAVTntuHit", 500);
+      arr->SetOwner(true);
+      fListOfPixels->AddAt(arr, i);
    }
-   m_listOfPixels->SetOwner(true);
+   fListOfPixels->SetOwner(true);
+   fMap.clear();
 }
-
-
-
-
 
 //------------------------------------------+-----------------------------------
 //! Clear event.
-void TAVTntuRaw::Clear(Option_t*) {
-
-    for (Int_t i = 0; i < m_vtxGeo->GetNSensors(); ++i) {
-        TClonesArray* list = GetListOfPixels(i);
-        list->Delete();   
-        list->Clear();
-        // list->Clear("C");
-    }
-
-    m_mcHitList.clear();
-    m_mcClusterList.clear();
-    m_noiseList.clear();
-    m_pileUpList.clear();
-
+void TAVTntuRaw::Clear(Option_t*)
+{
+   for (Int_t i = 0; i < fpGeoMap->GetNSensors(); ++i) {
+      TClonesArray* list = GetListOfPixels(i);
+      list->Clear("C");
+   }
+   fMap.clear();
 }
 
+//______________________________________________________________________________
+//
+TAVTntuHit* TAVTntuRaw::NewPixel(Int_t iSensor, Double_t value, Int_t aLine, Int_t aColumn)
+{
+   if (iSensor >= 0  || iSensor < fpGeoMap->GetNSensors()) {
+      TClonesArray &pixelArray = *GetListOfPixels(iSensor);
+      std::pair<int, int> idx(aLine, aColumn);
+      
+      // check if pixel already exists
+      if ( fMap[idx] == iSensor+1) {
+         TAVTntuHit* pixel = new TAVTntuHit(iSensor, value, aLine, aColumn);
+         TAVTntuHit* curPixel = (TAVTntuHit*)pixelArray.FindObject(pixel);
+         delete pixel;
+         return curPixel;
+         
+      } else {
+         fMap[idx] = iSensor+1;
+         TAVTntuHit* pixel = new(pixelArray[pixelArray.GetEntriesFast()]) TAVTntuHit(iSensor, value, aLine, aColumn);
+         return pixel;
+      }
+   } else {
+      Error("NewPixel()", "Wrong sensor number %d\n", iSensor);
+      return 0x0;
+   }
+}
 
-
-
+//______________________________________________________________________________
+//
+TAVTntuHit* TAVTntuRaw::NewPixel(Int_t iSensor, TAVTrawHit* rawPixel)
+{
+   if (iSensor >= 0  || iSensor < fpGeoMap->GetNSensors()) {
+      TClonesArray &pixelArray = *GetListOfPixels(iSensor);
+      TAVTntuHit* pixel = new(pixelArray[pixelArray.GetEntriesFast()]) TAVTntuHit(iSensor, rawPixel);
+      return pixel;
+   } else {
+      Error("NewPixel()", "Wrong sensor number %d\n", iSensor);
+      return 0x0;
+   }
+}
 
 /*------------------------------------------+---------------------------------*/
 //! ostream insertion.
 void TAVTntuRaw::ToStream(ostream& os, Option_t* option) const
 {
-   // for (Int_t i = 0; i < m_vtxGeo->GetNSensors(); ++i) {
-	  
-	  // os << "TAVTntuRaw " << GetName()
-	  // << Form("  nPixels=%3d", GetPixelsN(i))
-	  // << endl;
-	  
-	  // //TODO properly
-	  // //os << "slat stat    adct    adcb    tdct    tdcb" << endl;
-	  // for (Int_t j = 0; j < GetPixelsN(i); j++) {  // all by default
-		 // const TAVTntuHit*  pixel = GetPixel(i,j, "all");
-		 // if (pixel)
-			// os << Form("%4d", pixel->GetPixelIndex());
-		 // os << endl;
-	  // }
-   // }
+   for (Int_t i = 0; i < fpGeoMap->GetNSensors(); ++i) {
+      
+      os << "TAVTntuRaw " << GetName()
+      << Form("  nPixels=%3d", GetPixelsN(i))
+      << endl;
+      
+      //TODO properly
+      //os << "slat stat    adct    adcb    tdct    tdcb" << endl;
+      for (Int_t j = 0; j < GetPixelsN(i); j++) {
+         const TAVTntuHit*  pixel = GetPixel(i,j);
+         if (pixel)
+            os << Form("%4d", pixel->GetPixelIndex());
+         os << endl;
+      }
+   }
 }
-
 
 
 
--- libs/src/TAVTbase/TAVTntuRaw.hxx
+++ libs/src/TAVTbase/TAVTntuRaw.hxx
@@ -7,78 +7,46 @@
  */
 /*------------------------------------------+---------------------------------*/
 
-
-#include "TAVTntuHit.hxx"
-
-#include "TAGdata.hxx"
 #include <map>
+#include "TAVTntuHit.hxx"
+#include "TAGdata.hxx"
 
-/*
-
-
-mc_hit      = only pixels coming from FLUKA ntuples (now just one hit per particle track)
-mc_cluster  = only px created in digitization from the mc_hit (original px coordinate already in, but it is a different object)
-noise       = only noise px signal
-pileup      = only pileup pixel signal
-forPhys     = mc_cluster + noise + pileup 
-all 
-
-
-*/
-
-
+class TAVTparGeo;
 class TAVTntuRaw : public TAGdata {
    
 private:
    //using TObjArray here
-   TObjArray*        m_listOfPixels; 
-   // static TString    fgkBranchName;    // Branch name in TTree
+   TObjArray*        fListOfPixels;
+   TAVTparGeo*       fpGeoMap;         //! do not store
    
-   TAVTparGeo* m_vtxGeo;
-
-    // vector of map of < Nsensor, listID >
-    map<int, vector<int> >      m_mcHitList;
-    map<int, vector<int> >      m_mcClusterList;
-    map<int, vector<int> >      m_noiseList;
-    map<int, vector<int> >      m_pileUpList;
-
-    // map< map<int, vector<int> > >       m_pixelIndexList;
-
-    int               GetPixelsN(int iSensor) const; 
-    TAVTntuHit*       NewPixel(Int_t sensor, Double_t value, Int_t aLine, Int_t aColumn); //  Deprecated, to be private
-
-public:
-
-    TAVTntuRaw();
-    virtual          ~TAVTntuRaw();
-
+    std::map<pair<int, int>, int > fMap; //!
     
-    TAVTntuHit*         NewPixel(Int_t sensor, TAVTrawHit* pixel);
-
-    TAVTntuHit*         NewPixel(int iSensor, double value, int aLine, int aColumn, string aorigin);
-    TAVTntuHit*         NewPixel(int iSensor, double value, int aLine, int aColumn, string aorigin, int amcID, int agenPartID);
-    TAVTntuHit*         NewPixel(int iSensor, double value, int aLine, int aColumn, string aorigin, TAVTntuHit* originatingHit );
-
-    void                FillPixelList(int iSensor, string command, int id );
-
-    TAVTntuHit*       GetPixel(Int_t iSensor, Int_t iPixel, string command );
-    const TAVTntuHit* GetPixel(Int_t iSensor, Int_t iPixel, string command ) const;
-
-    TClonesArray*     GetListOfPixels(Int_t iSensor);
-    TClonesArray*     GetListOfPixels(Int_t iSensor) const;
-
-    int               GetPixelsN( int iSensor, string command ); 
-    int               GetNSensors() { return m_vtxGeo->GetNSensors(); };
-
-    virtual void      SetupClones();
-
-    virtual void      Clear(Option_t* opt="");
-
-    // delete?
-    virtual void      ToStream(ostream& os=cout, Option_t* option="") const;
- 
-
-   // static const Char_t* GetBranchName()   { return fgkBranchName.Data();   }
+private:
+   static TString    fgkBranchName;    // Branch name in TTree
+   
+public:
+   TAVTntuRaw();
+   virtual          ~TAVTntuRaw();
+   
+   TAVTntuHit*       GetPixel(Int_t iSensor, Int_t iPixel);
+   const TAVTntuHit* GetPixel(Int_t iSensor, Int_t iPixel) const;
+   
+   TClonesArray*     GetListOfPixels(Int_t iSensor);
+   TClonesArray*     GetListOfPixels(Int_t iSensor) const;
+   
+   Int_t             GetPixelsN(Int_t iSensor) const;
+   
+   TAVTntuHit*       NewPixel(Int_t sensor, Double_t value, Int_t aLine, Int_t aColumn);
+   TAVTntuHit*       NewPixel(Int_t sensor, TAVTrawHit* pixel);
+   
+   virtual void      SetupClones();
+   
+   virtual void      Clear(Option_t* opt="");
+   
+   virtual void      ToStream(ostream& os=cout, Option_t* option="") const;
+   
+public:
+   static const Char_t* GetBranchName()   { return fgkBranchName.Data();   }
    
    ClassDef(TAVTntuRaw,1)
 };
