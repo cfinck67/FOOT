f473fdb0911ec60caebe11ab0f13463b41c6d5d9
Christian Finck <cfinck@iphc.cnrs.fr>
2018-07-20 15:33:09 +0200

    New version of digitizer (see talk 6 June 2018)

--- libs/src/TAVTbase/TAVTdigitizerE.cxx
+++ libs/src/TAVTbase/TAVTdigitizerE.cxx
@@ -1,151 +1,163 @@
-/*!
- \file
- \version $Id: TAVTdigitizerE.cxx,v 1.9 2003/06/22 10:35:48 mueller Exp $
- \brief   Implementation of TAVTdigitizerE.
- */
-#include "TMath.h"
-#include "TGeoMatrix.h"
-#include "TLegend.h"
-#include "TList.h"
-#include "TF1.h"
-#include "TStyle.h"
-#include "TRandom2.h"
-
-#include "TAVTparGeo.hxx"
 #include "TAVTdigitizerE.hxx"
+#include "TAVTparGeo.hxx"
 
-/*!
- \class TAVTdigitizerE 
- \brief digitizer for pixel **
- */
+#include <math.h>
+#include "TVector2.h"
+#include "TRandom.h"
+#include "TList.h"
 
 ClassImp(TAVTdigitizerE);
 
 using namespace std;
 
-Int_t   TAVTdigitizerE::fgkMaxTurn      = 10;
-Float_t TAVTdigitizerE::fgkFactorRad    = 1.71;
+Int_t   TAVTdigitizerE::fgkMaxTurn   = 16;
+Int_t   TAVTdigitizerE::fgkMeshWidth = 10; // test radius condition in a square of +/- mesh width
 
 //00
-Int_t   TAVTdigitizerE::fgkShel00[]     = {4,  12,    16,    32,    44,    52,    76,    88,   112,   120 };
-Int_t   TAVTdigitizerE::fgkDeltaRad00[] = {0,   0,     0,     0,     6,     0,     8,     8,    12,    10 };
-
-//55
-Int_t   TAVTdigitizerE::fgkShel55[]     = {5,   9,    13,    21,    25,    45,    69,   101,   109,   113 };
-Int_t   TAVTdigitizerE::fgkDeltaRad55[] = {0,   0,     0,     0,     0,     0,     0,     4,     8,     7 };
+Int_t   TAVTdigitizerE::fgkShel00[20];// {4,  12,  16,   24,   32,   44,   52,   76,   88,  112,   120,  148,  164,  188,  208,  248};
 
 //05
-Int_t   TAVTdigitizerE::fgkShel50[]     = {2,   8,    12,    26,    40,    44,    61,    82,    94,   104 };
-Int_t   TAVTdigitizerE::fgkDeltaRad50[] = {0,   0,     0,     0,     2,     0,     1,     4,     8,     4 };
+Int_t   TAVTdigitizerE::fgkShel50[20];// {2,   6,   8,   16,   26,   34,   44,   56,   74,   86,   104,  124,  142,  166,  188,  216};
 
-//------------------------------------------+-----------------------------------
-//! Default constructor.
+//55
+Int_t   TAVTdigitizerE::fgkShel55[20];// {1,   5,   9,   13,   21,   37,   45,   57,   69,   89,   101,  121,  137,  161,  185,  213};
+
+// --------------------------------------------------------------------------------------
 TAVTdigitizerE::TAVTdigitizerE(TAGparaDsc* parGeo)
-: TAVTbaseDigitizer(parGeo),
-  fDeltaRad(0x0)
+: TAVTbaseDigitizer(parGeo)
 {
-
+   DefineRadii();
 }
 
-//------------------------------------------+-----------------------------------
-//! Destructor.
-TAVTdigitizerE::~TAVTdigitizerE()
-{
-
+// --------------------------------------------------------------------------------------
+TAVTdigitizerE::~TAVTdigitizerE() 
+{   
 }
 
 // --------------------------------------------------------------------------------------
 Bool_t TAVTdigitizerE::MakeCluster(Double_t x0, Double_t y0, Double_t /*zin*/, Double_t /*zout*/)
 {
    // std array
-   TAVTparGeo* pGeoMap  = (TAVTparGeo*) fpParGeo->Object();
-
    fMap.clear();
-   fDistance.clear();
-   std::pair<int, float> pairDistance;
-   
-   if (TMath::Abs(x0) > fPitchX*fPixelsNx/2.) {
-      // if ( GlobalPar::GetPar()->Debug() > 0 )   
-         cout << "WARNING :: TAVTdigitizerE::MakeCluster  -->  TMath::Abs(x0) > fPitchX*fPixelsNx/2 is false. x0 = " << TMath::Abs(x0) << " fPitchX "<<fPitchX<<" fPixelsNx "<<fPixelsNx<< endl;
-      return false;
-   }
-   if (TMath::Abs(y0) > fPitchY*fPixelsNy/2.) { 
-      // if ( GlobalPar::GetPar()->Debug() > 0 )   
-         cout << "WARNING :: TAVTdigitizerE::MakeCluster  -->  TMath::Abs(y0) > fPitchY*fPixelsNy/2 is false. y0 = "  << TMath::Abs(y0) << " fPitchY "<<fPitchY<<" fPixelsNy "<<fPixelsNy<< endl;
-      return false;
-   }
+
+   if (TMath::Abs(y0) > fPitchX*fPixelsNx/2.) return false;
+   if (TMath::Abs(x0) > fPitchY*fPixelsNy/2.) return false;
    
    Int_t line0 = GetLine(y0);
    Int_t col0  = GetColumn(x0);
-   
+
    if (fDebugLevel)
       printf("%d %d %d\n", fPixelsN, line0, col0);
    
-   // why this? WE already have fPitchX fPitchY...
    Float_t pitchX  = GetPitchX();
    Float_t pitchY  = GetPitchY();
-   
+
    // choose the cluster version
-   if (!SetRegion(x0, y0)) {
-      cout << "WARNING :: TAVTdigitizerE::MakeCluster  -->  SetRegion false" << endl;
+   if (!SetRegion(x0, y0))
       return false;
+   
+   Int_t regX  = GetColRegion(x0);
+   Int_t regY  = GetLineRegion(y0);
+   if (regX == 5 && regY == 5) {
+      // rounding pb
+      x0 += (x0 > 0) ? -1 : +1;
+      y0 += (y0 > 0) ? -1 : +1;
    }
-      
    
    // define radii
    Int_t rpixels      = fPixelsN;
+  // printf("pixels %d\n", fPixelsN);
    Int_t lastTurn     = GetLastShell(fShel, fgkMaxTurn);
    Int_t shellPixels  = fShel[lastTurn];
    rpixels            = shellPixels - rpixels;
+
+   if (rpixels != 0)lastTurn--;
+
+   // radius
+   Int_t closed  = fShel[lastTurn];
+   Int_t valence = fShel[lastTurn+1];
    
-   Int_t sigma        = Int_t(TMath::Sqrt(shellPixels)/2.+1);
-   Float_t radiusLim  = TMath::Sqrt(shellPixels - fDeltaRad[lastTurn])/fgkFactorRad*pitchX;
+   Float_t radiusLim  = (*fMapRadius)[closed];
+   Float_t radiusLim1 = (*fMapRadius)[valence];
+   Int_t sigma        = (radiusLim1/pitchY)*1.5;
    
    // fill map
    // x1 - y1
    Int_t col1  = col0  - sigma;
    Int_t line1 = line0 + sigma;
-   Float_t x1  = pGeoMap->GetPositionU(col1);
-   Float_t y1  = pGeoMap->GetPositionV(line1);
+   
+   Float_t x1  = GetPositionU(col1);
+   Float_t y1  = GetPositionV(line1);
    
    // x2 - y2
    Int_t col2  = col0  + sigma;
    Int_t line2 = line0 - sigma;
-   Float_t x2  = pGeoMap->GetPositionU(col2);
-   Float_t y2  = pGeoMap->GetPositionV(line2);
+   
+   Float_t x2  = GetPositionU(col2);
+   Float_t y2  = GetPositionV(line2);
    
    if (fDebugLevel) {
       printf("%.1f %.1f %d %d\n", x1, x2, col1, line1);
       printf("%.1f %.1f %d %d\n", y1, y2, col2, line2);
    }
    
+   std::vector<int> rarray;
+   
    for (Float_t y = y1; y <= y2+pitchY/4.; y+= pitchY) {
       for (Float_t x = x1; x <= x2+pitchX/4.; x+= pitchX) {
          
-         Int_t line   = GetLine(y);
-         Int_t col    = GetColumn(x);
-         Int_t idx    = GetIndex(line, col);
-         Float_t A    = (x-x0)/radiusLim;
-         Float_t B    = (y-y0)/radiusLim;
+         Int_t line = GetLine(y);
+         Int_t col  = GetColumn(x);
+         Int_t idx  = GetIndex(line, col);
+         if (idx < 0) continue;
+         
+         Float_t A    = (x-x0);
+         Float_t B    = (y-y0);
          Float_t rad2 =  A*A + B*B;;
          
-         if (rad2 < 1.) {
-            pairDistance.first  = idx;
-            pairDistance.second = TMath::Sqrt(rad2);
+         // inner shell
+         if (rad2 <= radiusLim*radiusLim)
             fMap[idx] = 1;
-            fDistance.push_back(pairDistance);
+         
+         if (rpixels != 0) {
+            // from inner shell to next closed shell
+            if (rad2 > radiusLim*radiusLim && rad2 < radiusLim1*radiusLim1) {
+               fMap[idx] = 1;
+               rarray.push_back(idx);
+            }
          }
       }
    }
    
-   // remove pixels until the pixels number is reached
-   RemovePixels(rpixels);
+   // remove randomly pixels from outer shell
+   Int_t r = 0;
+   if (rpixels != 0) {
+      
+      std::random_shuffle(rarray.begin(), rarray.end(), GetRandom);
+      
+      for (std::vector<int>::iterator it=rarray.begin(); it!=rarray.end(); ++it) {
+         
+         if (r > rpixels-1)
+            break;
+         
+         fMap.erase(*it);
+         r++;
+      }
+   }
    
-   return true;}
+   if (fDebugLevel) {
+      printf("pixels input %d\n", fPixelsN);
+      printf("shell close  : %d r %4.2f\n", closed, (*fMapRadius)[closed]);
+      printf("shell valence: %d r %4.2f\n", valence, (*fMapRadius)[valence]);
+      printf("sigma %d\n", sigma);
+      printf("Cluster size %lu\n", fMap.size());
+   }
 
+   return true;
+}
 
 //_____________________________________________________________________________
-Bool_t TAVTdigitizerE::SetRegion(Float_t x0, Float_t y0)
+Bool_t TAVTdigitizerE::SetRegion(Double_t x0, Double_t y0)
 {
    Int_t regX  = GetColRegion(x0);
    Int_t regY  = GetLineRegion(y0);
@@ -193,21 +205,23 @@ Bool_t TAVTdigitizerE::SetRegion(Float_t x0, Float_t y0)
    }
    
    if (regX == 5 && regY == 5) {
-      fShel     = fgkShel55;
-      fDeltaRad = fgkDeltaRad55;
+      fShel      = fgkShel55;
+      fMapRadius = &fMapRadius55;
+      // rounding pb
+      x0 += (x0 > 0) ? -1 : +1;
+      y0 += (y0 > 0) ? -1 : +1;
       
    } else if ((regX == 0 && regY == 0) || (regX == 1 && regY == 1) || (regX == 1 && regY == 0) || (regX == 0 && regY == 1)) {
-      fShel     = fgkShel00;
-      fDeltaRad = fgkDeltaRad00;
-      
-      
+      fShel      = fgkShel00;
+      fMapRadius = &fMapRadius00;
+
    } else if ((regX == 5 && regY == 0) || (regX == 5 && regY == 1)) {
-      fShel     = fgkShel50;
-      fDeltaRad = fgkDeltaRad50;
+      fShel      = fgkShel50;
+      fMapRadius = &fMapRadius50;
       
    } else if ((regX == 0 && regY == 5) || (regX == 1 && regY == 5)) {
-      fShel     = fgkShel50;
-      fDeltaRad = fgkDeltaRad50;
+      fShel      = fgkShel50;
+      fMapRadius = &fMapRadius50;
       
    } else
       return false;
@@ -216,33 +230,88 @@ Bool_t TAVTdigitizerE::SetRegion(Float_t x0, Float_t y0)
 }
 
 //_____________________________________________________________________________
-void TAVTdigitizerE::RemovePixels(Int_t rpixels)
+void TAVTdigitizerE::DefineRadii()
 {
-   // remove pixels
-   if (rpixels != 0) {
+   DefineRadius00();
+   DefineRadius50();
+   DefineRadius55();
+}
+
+//_____________________________________________________________________________
+void TAVTdigitizerE::DefineRadius55()
+{
+   Float_t pitchX = GetPitchX();
+   Float_t pitchY = GetPitchY();
+   
+   Int_t limx = fgkMeshWidth;
+   Int_t limy = fgkMeshWidth;
+   Int_t turn = 0;
+   
+   for (Float_t r = 0.7*pitchX; r < 8.6*pitchX; r += 0.5*pitchX) {
       
-      std::vector<int> rarray;
+      Int_t n   = 0;
       
-      for (Int_t i = 0; i < int(rpixels*1.3); ++i) {
-         if (i > rpixels-1)
-            rarray.push_back(0);
-         else
-            rarray.push_back(1);
-      }
-      
-      std::random_shuffle(rarray.begin(), rarray.end(), GetRandom);
-      
-      Int_t r = 0;
-      std::sort(fDistance.begin(), fDistance.end(), SortBack);
-      
-      for (std::vector<std::pair<int, float> >::iterator it=fDistance.begin(); it!=fDistance.end(); ++it) {
-         if (rarray[r] == 1) {
-            fMap.erase(it->first);
+      for (int i = -limx ; i <= limx ; i++) {
+         for (int j = -limy ; j <= limy ; j++) {
+            if ((i*pitchX*i*pitchX) + (j*pitchY*j*pitchY) < r*r) {
+               n++;
+            }
          }
-         if (r > rpixels+1)
-            break;
-         r++;
       }
+      fgkShel55[turn++] = n;
+      fMapRadius55[n] = r;
+   }
+}
+
+//_____________________________________________________________________________
+void TAVTdigitizerE::DefineRadius50()
+{
+   Float_t pitchX = GetPitchX();
+   Float_t pitchY = GetPitchY();
+   
+   Int_t limx = fgkMeshWidth;
+   Int_t limy = fgkMeshWidth;
+   Int_t turn = 0;
+   
+   for (Float_t r = 0.75*pitchX; r <= 8.75*pitchX; r += 0.5*pitchX) {
+      
+      Int_t n   = 0;
+      
+      for (int i = -limx ; i <= limx ; i++) {
+         for (int j = -limy ; j <= limy ; j++) {
+            if (((i-0.5)*pitchX*(i-0.5)*pitchX) + ((j)*pitchY*(j)*pitchY) < r*r) {
+               n++;
+            }
+         }
+      }
+      fgkShel50[turn++] = n;
+      fMapRadius50[n] = r;
+   }
+}
+
+//_____________________________________________________________________________
+void TAVTdigitizerE::DefineRadius00()
+{
+   Float_t pitchX = GetPitchX();
+   Float_t pitchY = GetPitchY();
+   
+   Int_t limx = fgkMeshWidth;
+   Int_t limy = fgkMeshWidth;
+   Int_t turn = 0;
+   
+   for (Float_t r = 1.25*pitchX; r <= 9.25*pitchX; r += 0.5*pitchX) {
+      
+      Int_t n   = 0;
+      
+      for (int i = -limx ; i <= limx ; i++) {
+         for (int j = -limy ; j <= limy ; j++) {
+            if (((i-0.5)*pitchX*(i-0.5)*pitchX) + ((j-0.5)*pitchY*(j-0.5)*pitchY) < r*r) {
+               n++;
+            }
+         }
+      }
+      fgkShel00[turn++] = n;
+      fMapRadius00[n] = r;
    }
 }
 
