ddfd2c5a069758e2a17b0aa65648831212cb6c12
Christian Finck <cfinck@iphc.cnrs.fr>
2018-08-21 14:29:45 +0200

    Using VTX base class

--- libs/src/TAITbase/TAITactNtuClusterF.cxx
+++ libs/src/TAITbase/TAITactNtuClusterF.cxx
@@ -8,8 +8,11 @@
 #include "TH2F.h"
 #include "TMath.h"
 
-#include "TAITparGeo.hxx"
-#include "TAITparConf.hxx"
+#include "TAVTdatRaw.hxx"
+#include "TAVTparGeo.hxx"
+#include "TAVTparConf.hxx"
+#include "TAITntuRaw.hxx"
+#include "TAITntuRaw.hxx"
 #include "TAITntuCluster.hxx"
 #include "TAITactNtuClusterF.hxx"
 
@@ -25,9 +28,12 @@ ClassImp(TAITactNtuClusterF);
 TAITactNtuClusterF::TAITactNtuClusterF(const char* name, 
 									 TAGdataDsc* pNtuRaw, TAGdataDsc* pNtuClus,
 									 TAGparaDsc* pConfig, TAGparaDsc* pGeoMap)
-: TAVTactNtuClusterF(name, pNtuRaw, pNtuClus, pConfig, pGeoMap)
+: TAVTactBaseNtuCluster(name, pConfig, pGeoMap),
+  fpNtuRaw(pNtuRaw),
+  fpNtuClus(pNtuClus)
 {
-
+   AddDataIn(pNtuRaw,   "TAITntuRaw");
+   AddDataOut(pNtuClus, "TAITntuCluster");
 }
 
 //------------------------------------------+-----------------------------------
@@ -36,7 +42,28 @@ TAITactNtuClusterF::~TAITactNtuClusterF()
 {
    
 }
-										
+
+//______________________________________________________________________________
+//
+Bool_t TAITactNtuClusterF::Action()
+{
+   TAITntuRaw* pNtuHit  = (TAITntuRaw*) fpNtuRaw->Object();
+   TAVTparConf* pConfig = (TAVTparConf*) fpConfig->Object();
+   
+   Bool_t ok = true;
+   
+   for (Int_t i = 0; i < pConfig->GetSensorsN(); ++i) {
+      fListOfPixels = pNtuHit->GetListOfPixels(i);
+      if (fListOfPixels->GetEntries() > pConfig->GetAnalysisPar().HitsInPlaneMaximum) continue;
+      if (fListOfPixels->GetEntries() == 0) continue;
+      ok += FindClusters(i);
+   }
+   
+   if(ok)
+      fpNtuClus->SetBit(kValid);
+   return ok;
+}
+
 //______________________________________________________________________________
 //  
 Bool_t TAITactNtuClusterF::FindClusters(Int_t iSensor)
@@ -45,7 +72,112 @@ Bool_t TAITactNtuClusterF::FindClusters(Int_t iSensor)
    // Look in a iterative way to next neighbour
    
    TAITntuCluster* pNtuClus = (TAITntuCluster*) fpNtuClus->Object();
-   TAITparGeo*     pGeoMap  = (TAITparGeo*)     fpGeoMap->Object();
+   TAVTparGeo*     pGeoMap  = (TAVTparGeo*)     fpGeoMap->Object();
+
+   FillMaps(pGeoMap);
+   SearchCluster(pGeoMap);
+ 
+   return CreateClusters(iSensor, pNtuClus, pGeoMap);
+}
+
+//______________________________________________________________________________
+//
+Bool_t TAITactNtuClusterF::CreateClusters(Int_t iSensor, TAITntuCluster* pNtuClus, TAVTparGeo* pGeoMap)
+{
+   Int_t nLine = pGeoMap->GetNPixelY()+1;
+   Int_t nCol  = pGeoMap->GetNPixelX()+1;
    
-   return TAVTactNtuClusterF::FindClusters(iSensor, pNtuClus, pGeoMap);
+   TAITcluster* cluster = 0x0;
+
+   // create clusters
+   for (Int_t i = 0; i< fClustersN; ++i)
+      pNtuClus->NewCluster(iSensor);
+   
+   for (Int_t iPix = 0; iPix < fListOfPixels->GetEntries(); ++iPix) {
+      TAITntuHit* pixel = (TAITntuHit*)fListOfPixels->At(iPix);
+      Int_t line = pixel->GetPixelLine();
+      Int_t col  = pixel->GetPixelColumn();
+      if(line >= nLine) continue;
+      if(col >= nCol) continue;
+      if( line < 0) continue;
+      if( col < 0) continue;
+      
+      Int_t clusterN = fFlagMap[line*nCol+col];
+      if ( clusterN != -1 ) {
+         cluster = pNtuClus->GetCluster(iSensor, clusterN);
+         cluster->AddPixel(pixel);
+      }
+   }
+   
+   // Compute position and fill clusters info
+   for (Int_t i = 0; i< pNtuClus->GetClustersN(iSensor); ++i) {
+      cluster = pNtuClus->GetCluster(iSensor, i);
+      cluster->SetPlaneNumber(iSensor);
+      fPSeed = cluster->GetPixel(0);
+      fCurListOfPixels = cluster->GetListOfPixels();
+      ComputePosition();
+      cluster->SetNumber(i);
+      TVector3 posG = *GetCurrentPosition();
+      pGeoMap->Sensor2Detector_frame(iSensor, &posG);
+      //  pGeoMap->Local2Global(&posG);
+      cluster->SetPositionG(&posG);
+      cluster->SetPosition(GetCurrentPosition());
+      cluster->SetPosError(GetCurrentPosError());
+      cluster->SetIndexSeed(fPSeed->GetPixelIndex());
+      
+      if (ApplyCuts(cluster)) {
+         // histogramms
+         if (ValidHistogram()) {
+            if (cluster->GetPixelsN() > 0) {
+               fpHisPixelTot->Fill(cluster->GetPixelsN());
+               fpHisPixel[iSensor]->Fill(cluster->GetPixelsN());
+               // printf("sensor %d %d\n", iSensor, cluster->GetPixelsN());
+               if (TAVTparConf::IsMapHistOn()) {
+                  fpHisClusMap[iSensor]->Fill(cluster->GetPosition()[0], cluster->GetPosition()[1]);
+               }
+            }
+         }
+      } else {
+         pNtuClus->GetListOfClusters(iSensor)->Remove(cluster);
+         pNtuClus->GetListOfClusters(iSensor)->Compress();
+      }
+   }
+   if (pNtuClus->GetClustersN(iSensor))
+      return true;
+   
+   return false;
+
+}
+
+//______________________________________________________________________________
+//  
+Bool_t TAITactNtuClusterF::ShapeCluster(Int_t noClus, Int_t IndX, Int_t IndY, TAVTparGeo* pGeoMap)
+{
+   
+   Int_t nLine = pGeoMap->GetNPixelY()+1;
+   Int_t nCol  = pGeoMap->GetNPixelX()+1;
+
+   if ( fPixelMap[IndX*nCol+IndY] <= 0 ) return false;
+    if ( fFlagMap[IndX*nCol+IndY] != -1 ) return false;
+   fFlagMap[IndX*nCol+IndY] = noClus;
+
+   TAVTntuHit* pixel = (TAVTntuHit*)GetListOfPixels()->At(fIndexMap[IndX*nCol+IndY]);
+   pixel->SetFound(true);
+      
+   for(Int_t i = -1; i <= 1 ; ++i)
+	  if ( IndX+i >= 0 && IndX+i < nLine)
+		 ShapeCluster(noClus, IndX+i, IndY, pGeoMap);
+   
+   for(Int_t j = -1; j <= 1 ; ++j)
+	  if ( IndY+j >= 0 && IndY+j < nCol)
+		 ShapeCluster(noClus, IndX  , IndY+j, pGeoMap);
+   
+   return true;
+}
+
+//______________________________________________________________________________
+//
+void TAITactNtuClusterF::ComputePosition()
+{
+   TAVTactBaseNtuCluster::ComputePosition();
 }
--- libs/src/TAITbase/TAITactNtuClusterF.hxx
+++ libs/src/TAITbase/TAITactNtuClusterF.hxx
@@ -6,29 +6,43 @@
  \brief   Declaration of TAITactNtuClusterF.
  */
 /*------------------------------------------+---------------------------------*/
-#include <map>
-#include "TArrayI.h"
 
-#include "TAVTactNtuClusterF.hxx"
+#include "TAVTactBaseNtuCluster.hxx"
 
-class TAVTcluster;
+class TAITntuCluster;
 class TAVTntuHit;
+class TAVTparGeo;
 
-class TAITactNtuClusterF : public TAVTactNtuClusterF {
+class TAITactNtuClusterF : public TAVTactBaseNtuCluster {
    
 public:
-   explicit  TAITactNtuClusterF(const char* name     =  0,
-								   TAGdataDsc* p_nturaw =  0, 
-								   TAGdataDsc* p_ntuclus = 0, 
-								   TAGparaDsc* p_config =  0,
-							      TAGparaDsc* p_geomap =  0);
+   explicit  TAITactNtuClusterF(const char* name     = 0,
+								       TAGdataDsc* p_nturaw  = 0,
+                               TAGdataDsc* p_ntuclus = 0,
+								       TAGparaDsc* p_config  = 0,
+							          TAGparaDsc* p_geomap  = 0);
    
    virtual ~TAITactNtuClusterF();
    
+   //! Action
+   virtual  Bool_t Action();
+
    //! Find cluster charge, noise & position
-   Bool_t  FindClusters(Int_t iSensor);
+   virtual Bool_t  FindClusters(Int_t iSensor);
    
+   //! Compute position
+   void    ComputePosition();
+
+private:
+   TAGdataDsc*     fpNtuRaw;		  // input data dsc
+   TAGdataDsc*     fpNtuClus;		  // output data dsc
    
+private:
+   Bool_t  ShapeCluster(Int_t noClus, Int_t IndX, Int_t IndY, TAVTparGeo* pGeoMap);
+   Bool_t  FindClusters(Int_t iSensor, TAITntuCluster* pNtuClus, TAVTparGeo* pGeoMap);
+   Bool_t  CreateClusters(Int_t iSensor, TAITntuCluster* pNtuClus, TAVTparGeo* pGeoMap);
+
+
    ClassDef(TAITactNtuClusterF,0)
 };
 
