387534ebcc5d14257c0a48cb76587988b88bce02
Christian Finck <cfinck@iphc.cnrs.fr>
2018-08-23 11:03:19 +0200

    Ordering strips

--- libs/src/TAMSDbase/TAMSDdigitizer.cxx
+++ libs/src/TAMSDbase/TAMSDdigitizer.cxx
@@ -18,6 +18,10 @@ Float_t TAMSDdigitizer::fgChargeFac       = 0.8;
 TAMSDdigitizer::TAMSDdigitizer(TAVTbaseParGeo* parGeo)
 : TAVTbaseDigitizer(parGeo)
 {
+   fPitchX   = fpParGeo->GetPitchX();
+   fPixelsNx = fpParGeo->GetNPixelX();
+   fPitchY   = fpParGeo->GetPitchY();
+   fPixelsNy = fpParGeo->GetNPixelY();
 }
 
 // --------------------------------------------------------------------------------------
@@ -29,19 +33,30 @@ TAMSDdigitizer::~TAMSDdigitizer()
 //! fill pixel signal
 Bool_t TAMSDdigitizer::Process( Double_t edep, Double_t x0, Double_t y0,  Double_t zin, Double_t zout)
 {
-   if (fgSmearFlag) {
-      Float_t dx = gRandom->Gaus(0, fPitchX/2.);
-      Float_t dy = gRandom->Gaus(0, fPitchX/2.);
-      x0 += dx;
-      y0 += dy;
-   }
+//   if (fgSmearFlag) {
+//      Float_t dx = gRandom->Gaus(0, fPitchX/2.);
+//      Float_t dy = gRandom->Gaus(0, fPitchX/2.);
+//      x0 += dx;
+//      y0 += dy;
+//   }
 
    fMap.clear();
+   
+   Double_t value = edep*fgChargeGain*fgChargeFac;
    
    Int_t line = GetLine(y0);
    Int_t col  = GetColumn(x0);
    
-   Double_t value = edep*fgChargeGain*fgChargeFac;
+   if (line-1 >= 0) {
+      Float_t value = edep*fgChargeGain*(1-fgChargeFac/2.);
+      FillMap(line-1, col, value);
+   }
+   
+   if (col-1 >= 0) {
+      Float_t value = edep*fgChargeGain*(1-fgChargeFac/2.);
+      FillMap(line, col-1, value);
+   }
+   
    FillMap(line, col, value);
    
    if (line+1 < fPixelsNx) {
@@ -54,16 +69,6 @@ Bool_t TAMSDdigitizer::Process( Double_t edep, Double_t x0, Double_t y0,  Double
       FillMap(line, col+1, value);
    }
    
-   if (line-1 >= 0) {
-      Float_t value = edep*fgChargeGain*(1-fgChargeFac/2.);
-      FillMap(line-1, col, value);
-   }
-   
-   if (col-1 >= 0) {
-      Float_t value = edep*fgChargeGain*(1-fgChargeFac/2.);
-      FillMap(line, col-1, value);
-   }
-   
    return true;
 }
 
@@ -74,7 +79,6 @@ void TAMSDdigitizer::FillMap(Int_t line, Int_t col, Double_t value)
    Int_t idx  = GetIndex(line, col);
    if (idx < 0) return;
    fMap[idx] = value;
-
 }
 
 
