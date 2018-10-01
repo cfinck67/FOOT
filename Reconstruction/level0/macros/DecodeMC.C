8d09055c36486759545817537a944b9276ac9d42
Christian Finck <cfinck@iphc.cnrs.fr>
2018-10-01 16:06:41 +0200

    Add first version of DecodeMC macro

---
+++ Reconstruction/level0/macros/DecodeMC.C
@@ -0,0 +1,55 @@
+// Macro to make local reconstruction
+// Ch. Finck
+
+
+#if !defined(__CINT__) || defined(__MAKECINT__)
+
+#include <TString.h>
+#include <TStopwatch.h>
+
+#include "GlobalPar.hxx"
+#include "LocalRecoMC.hxx"
+
+#endif
+
+
+//void ReadVtxRawMC(TString name = "16O_C2H4_200_1.root")
+//void ReadVtxRawMC(TString name = "p_80_vtx.root")
+//void ReadVtxRawMC(TString name = "12C_80_vtx.root")
+void DecodeMC(TString name = "12C_400_vtx.root")
+{
+   GlobalPar::Instance();
+   GlobalPar::GetPar()->Print();
+   
+   Int_t pos = name.Last('.');
+   TString nameOut = name(0, pos);
+   nameOut.Append("_Out.root");
+   
+   LocalRecoMC* locRec = new LocalRecoMC(name, nameOut);
+   
+   // global setting
+   //locRec->EnableTree();
+   locRec->EnableHisto();
+   
+   // set detecteors
+   // VTX
+   locRec->EnableVtx();
+   locRec->EnableVtxTrack();
+   
+   // IT
+   locRec->EnableIt();
+   
+   // MSD
+   locRec->EnableMsd();
+
+   TStopwatch watch;
+   watch.Start();
+   
+   locRec->BeforeEventLoop();
+   locRec->LoopEvent(1);
+   locRec->AfterEventLoop();
+
+   watch.Print();
+}
+
+
