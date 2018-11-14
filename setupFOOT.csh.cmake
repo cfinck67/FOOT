6e0995a1a300b368666fadccc3ddf81e691b0971
Christian Finck <cfinck@iphc.cnrs.fr>
2018-11-14 12:43:17 +0100

    Do overwrite library path

--- setupFOOT.csh.cmake
+++ setupFOOT.csh.cmake
@@ -8,9 +8,9 @@ setenv FOOTRES $FOOTLEVEL0/results
 setenv FOOTSIMU $FOOTMAIN/Simulation
 
 if ($OSTYPE == "darwin") then
-  setenv DYLD_LIBRARY_PATH $ROOTSYS/lib:./:$ASOFTREF/lib
+  setenv DYLD_LIBRARY_PATH $ROOTSYS/lib:./:$ASOFTREF/lib:${DYLD_LIBRARY_PATH}
 else
-  setenv LD_LIBRARY_PATH $ROOTSYS/lib:./:$ASOFTREF/lib:
+  setenv LD_LIBRARY_PATH $ROOTSYS/lib:./:$ASOFTREF/lib:${LD_LIBRARY_PATH}
 endif
 
 set path = (${FOOTMAIN}/bin $path)
--- setupFOOT.sh.cmake
+++ setupFOOT.sh.cmake
@@ -10,9 +10,9 @@ export FOOTSIMU=$FOOTMAIN/Simulation
 
 if [ "$OSTYPE" = "darwin" ]
 then
-  export DYLD_LIBRARY_PATH=$ROOTSYS/lib:./:$ASOFTREF/lib
+  export DYLD_LIBRARY_PATH=$ROOTSYS/lib:./:$ASOFTREF/lib:${DYLD_LIBRARY_PATH}
 else
-  export LD_LIBRARY_PATH=$ROOTSYS/lib:./:$ASOFTREF/lib
+  export LD_LIBRARY_PATH=$ROOTSYS/lib:./:$ASOFTREF/lib:${LD_LIBRARY_PATH}
 fi
 
 export PATH=$PATH:$FOOTMAIN/bin
