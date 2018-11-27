730724802089a565dc50e2e24ac56c2a7ebbadbf
Christian Finck <cfinck@iphc.cnrs.fr>
2018-11-27 11:26:03 +0100

    Add some functionality

--- libs/src/TAGbase/TAGparTools.cxx
+++ libs/src/TAGbase/TAGparTools.cxx
@@ -21,7 +21,8 @@ ClassImp(TAGparTools);
 TAGparTools::TAGparTools()
 : TAGpara(),
   fFileStream(),
-  fFileName()
+  fFileName(),
+  fDebugLevel(0)
 {
    // Standard constructor
 }
@@ -39,6 +40,7 @@ void TAGparTools::ReadItem(TString& item)
    Char_t buf[255];
    TString key;
    while (pos == -1) {
+      if (fFileStream.eof()) break;
 	  fFileStream.getline(buf, 255);
 	  key = buf;
 	  pos = key.First(":");
@@ -69,20 +71,106 @@ void TAGparTools::ReadItem(Float_t &arg)
 }
 
 //_____________________________________________________________________________
-void TAGparTools::ReadItem(Float_t* coeff, Int_t size,  const Char_t delimiter)
+void TAGparTools::ReadItem(TArrayC& array, const Char_t delimiter)
 {
+   // From a string of the form "i-j;k;l;m-n" returns an integer array
+   // containing all the integers from i to j, then k, l and then from m to n.
+   
+   array.Set(3000);
+   array.Reset(0);
+   
    TString key;
    ReadItem(key);
    key = Normalize(key.Data());
+   if (key.Atoi() == -1)
+      return;
+   
+   // Get substrings separated by 'delimiter'
+   TObjArray* ranges = key.Tokenize(delimiter);
+   
+   // Finally takes each substring (which ought to be a range of the form
+   // x-y), and decode it into the theList integer vector.
+   for (Int_t i = 0; i < ranges->GetEntriesFast(); ++i ) {
+	 
+      TString& s = ((TObjString*)ranges->At(i))->String();
+      FillArray(s, array);
+   }
+   
+   delete ranges;
+}
+
+//_____________________________________________________________________________
+void TAGparTools::FillArray(TString& s, TArrayC& array)
+{
+   Int_t m1;
+   Int_t m2;
+   Int_t n;
+   Int_t incr;
+   GetRange(s.Data(),m1,m2,incr,n);
+   Int_t m = m1;
+   while ( n > 0 ) {
+      array[m] = 1;
+      m += incr;
+      --n;
+   }
+}
+
+//_____________________________________________________________________________
+void TAGparTools::ReadItem(TArrayC& arrayLine, TArrayC& arrayCol, const Char_t delimiter1, const Char_t delimiter2)
+{
+   // From a string of the form "i;j,k;l,m;n" returns an integer array
+   // containing the pixel line i / col j, line k / col l, line m / col n
+   
+   arrayLine.Reset(0);   
+   arrayCol.Reset(0);
+   
+   TString key;
+   ReadItem(key);
+   key = Normalize(key.Data());
+   if (key.Atoi() == -1)
+      return;
+
+   // Get substrings separated by 'delimiter2'
+   TObjArray* ranges = key.Tokenize(delimiter2);
+   
+   // Finally takes each substring (line and column number)
+   // and decode it into the theList integer vector.
+   for (Int_t i = 0; i < ranges->GetEntriesFast(); ++i ) {
+      TString& s       = ((TObjString*)ranges->At(i))->String();
+      TObjArray* pixel = s.Tokenize(delimiter1);
+      
+      if (pixel->GetEntriesFast() != 2)
+         Error("ReadItem()","wrong tokenize for [%s] with size %d", key.Data(), pixel->GetEntriesFast());
+
+      TString& sl    = ((TObjString*)pixel->At(0))->String();
+      TString& sc    = ((TObjString*)pixel->At(1))->String();
+      FillArray(sl, arrayLine);
+      FillArray(sc, arrayCol);
+      
+      delete pixel;
+   }
+   
+   delete ranges;
+}
+
+//_____________________________________________________________________________
+void TAGparTools::ReadItem(Float_t* coeff, Int_t size,  const Char_t delimiter)
+{
+   TString key;
+   TAGparTools::ReadItem(key);
    
    TObjArray* list = key.Tokenize(delimiter);
    if (list->GetEntries() != size)
-	  Error("ReadItem(Float*, Int_t, const Char_t)", "wrong tokenize");
-
+	  Error("ReadItem()","wrong tokenize for [%s] with size %d", key.Data(), size);
    
    for (Int_t k = 0; k < list->GetEntries(); k++) {
 	  TObjString* obj = (TObjString*)list->At(k);
 	  TString item = obj->GetString();
+     item =  Normalize(item);
+      Int_t pos = item.First(" ");
+      if (pos == -1)
+         pos = item.Length();
+      TString value(item(0, pos));      
 	  coeff[k] = item.Atof();
    }
    
@@ -105,29 +193,54 @@ void TAGparTools::ReadVector3(TVector3 &arg)
 }
 
 //_____________________________________________________________________________
-void TAGparTools::ReadStrings(Char_t *aString, Int_t aLength)
+void TAGparTools::ReadStrings(TString& aString)
 {
    
    // reads a string of a given max length from configuration file
    // The strings is expected to be contained by double quotes : "
    
    TString key;
-   ReadItem(key);
+   TAGparTools::ReadItem(key);
    
    Int_t pos = key.First('"');
    Int_t end = key.Last('"');
    
-   if (end == -1 || pos == -1 || end-pos-1 > aLength) {
-	  cout << "TAVTparGeo: problem with reading configuration file, missing \" or line too long" << endl; 
+   if (end == -1 || pos == -1 || end-pos-1 > 255) {
+	  cout << "TAGparTools: problem with reading file, missing \" or line too long" << endl;
 	  return;
    }
    
-   TString item(key(pos+1, end-pos-1));
-   strcpy(aString, item.Data());
-   aString[item.Length()] = '\0';
+    aString = key(pos+1, end-pos-1);
    
    if (fDebugLevel > 1) 
 	  cout << aString << endl;
+}
+
+//_____________________________________________________________________________
+void TAGparTools::GetRange(const char* cstr, Int_t& begin, Int_t& end, Int_t& incr, Int_t& n)
+{
+   // From a string of the form "m-n" returns a range (begin,end),
+   // its ordering (incr=+-1) and its size (abs(begin-end)+1)
+   
+   TString str(cstr);
+   
+   incr = 1;
+   Ssiz_t pos = str.First('-');
+   
+   if ( pos < 0 ) {
+	  begin = str.Atoi();
+	  end = -1;
+	  n = 1;
+   } else {
+	  begin = str.Atoi();
+	  end = TString(str(pos+1,str.Length()-pos)).Atoi();
+	  if ( begin > end ) {
+		 incr = -1;
+		 n = begin-end+1;
+	  } else {
+		 n = end-begin+1;
+	  }    
+   }
 }
 
 //_____________________________________________________________________________
@@ -170,11 +283,28 @@ Bool_t TAGparTools::Open(const TString& name)
    
    fFileStream.open(name.Data());
    if (!fFileStream) {
-	  Error("FromFile()", "failed to open file '%s'", name.Data());
-	  return kTRUE;
+	  Error("Open()", "failed to open file '%s'", name.Data());
+	  return false;
    }
    
    fFileName = name;
    
-   return kFALSE;
+   return true;
 }
+
+//______________________________________________________________________________
+Bool_t TAGparTools::Eof()
+{
+   // closing file
+   return fFileStream.eof();
+}
+
+//______________________________________________________________________________
+Bool_t TAGparTools::Close()
+{
+   // closing file
+   fFileStream.close();
+   
+   return true;
+}
+
--- libs/src/TAGbase/TAGparTools.hxx
+++ libs/src/TAGbase/TAGparTools.hxx
@@ -12,6 +12,7 @@
 
 #include "TObject.h"
 #include "TString.h"
+#include "TArrayC.h"
 #include "TVector3.h"
 
 #include "TAGpara.hxx"
@@ -24,24 +25,31 @@ class TAGparTools : public TAGpara {
 protected:
    ifstream fFileStream; // parameter file
    TString  fFileName;   // name of the file
+   Int_t    fDebugLevel; // debug level
    
 public:
-   
    TAGparTools();
    virtual ~TAGparTools();
    
    Bool_t  Open(const TString& name);
+   Bool_t  Eof();
+   Bool_t  Close();
    void    ReadItem(TString& item);
    void    ReadItem(Int_t &arg);
    void    ReadItem(Float_t &arg);
+   void    ReadItem(TArrayC& array, const Char_t delimiter = ';');
    void    ReadItem(Float_t* coeff, Int_t size, const Char_t delimiter = ' ');
+   void    ReadItem(TArrayC& arrayLine, TArrayC& arrayCol, const Char_t delimiter1 = ',', const Char_t delimiter2 = ';');
    void    ReadVector3(TVector3 &arg);
-   void    ReadStrings(Char_t *aString, Int_t aLength);
-   TString Normalize(const char* line);
+   void    ReadStrings(TString& aString);
+   void    GetRange(const char* str, Int_t& begin, Int_t& end, Int_t& incr, Int_t& n);
+   void    FillArray(TString& s, TArrayC& array);
 
+   TString Normalize(const char* line);
+   
    //! Get file name
    TString GetFileName() const { return fFileName; }
-
+   
    ClassDef(TAGparTools,1)
 };
 
