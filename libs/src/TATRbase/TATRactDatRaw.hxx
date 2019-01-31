488cbd80b314660ba2d2bbbbbd5c4cf834b5c2ff
Christian Finck <cfinck@iphc.cnrs.fr>
2019-01-31 12:49:09 +0100

    Interface with DAQ and DAQApi

--- libs/src/TATRbase/TATRactDatRaw.hxx
+++ libs/src/TATRbase/TATRactDatRaw.hxx
@@ -1,34 +1,38 @@
 #ifndef _TATRactDatRaw_HXX
 #define _TATRactDatRaw_HXX
 /*!
-  \file
-  \version $Id: TATRactDatRaw.hxx,v 1.3 2003/06/15 18:27:04 mueller Exp $
-  \brief   Declaration of TATRactDatRaw.
-*/
+ \file
+ \version $Id: TATRactDatRaw.hxx,v 1.3 2003/06/15 18:27:04 mueller Exp $
+ \brief   Declaration of TATRactDatRaw.
+ */
 /*------------------------------------------+---------------------------------*/
 
 #include "TAGaction.hxx"
 #include "TAGparaDsc.hxx"
 #include "TAGdataDsc.hxx"
 
+#include "TDCEvent.hh"
+
 class TATRactDatRaw : public TAGaction {
-  public:
-
-    explicit        TATRactDatRaw(const char* name=0,
-				 TAGdataDsc* p_datraw=0,
-				 TAGdataDsc* p_datmbs=0,
-				 TAGparaDsc* p_parmap=0);
-    virtual         ~TATRactDatRaw();
-
-    virtual Bool_t  Action();
-
-    ClassDef(TATRactDatRaw,0)
-
-  private:
-    TAGdataDsc*     fpDatRaw;		    // output data dsc
-    TAGdataDsc*     fpDatMbs;		    // input data dsc
-    TAGparaDsc*     fpParMap;		    // parameter dsc
-  
+public:
+   
+   explicit        TATRactDatRaw(const char* name=0,
+                                 TAGdataDsc* p_datraw=0,
+                                 TAGdataDsc* p_datqevt=0,
+                                 TAGparaDsc* p_parmap=0);
+   virtual        ~TATRactDatRaw();
+   
+   virtual         Bool_t  Action();
+   
+private:
+   void            DecodeHits(const TDCEvent* evt);
+   
+private:
+   TAGdataDsc*     fpDatRaw;		    // output data dsc
+   TAGdataDsc*     fpDaqEvent;		    // input data dsc
+   TAGparaDsc*     fpParMap;		    // parameter dsc
+   
+   ClassDef(TATRactDatRaw,0)
 };
 
 #endif
