981dae64913adcf8ddc9dd92fbc50cc6967d5395
Christian Finck <cfinck@iphc.cnrs.fr>
2019-02-27 17:49:31 +0100

    Don't need scrpt

--- libs/src/TASTbase/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find . -regex "\./TAST*[a-zA-Z0-9]*\.cxx" -exec sed -i "s/TATR/TAST/g" {} \;
-find . -regex "\./TAST*[a-zA-Z0-9]*\.hxx" -exec sed -i "s/TATR/TAST/g" {} \; 
-find . -regex "\./TAST*[a-zA-Z0-9]*\.icc" -exec sed -i "s/TATR/TAST/g" {} \;
-find . -regex "\./TAST*[a-zA-Z0-9]*\.h" -exec sed -i "s/TATR/TAST/g" {} \; 
