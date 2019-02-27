f5277c69ffff204f74916077bc4c592bb6d02b01
Christian Finck <cfinck@iphc.cnrs.fr>
2019-02-27 14:25:27 +0100

    Cosmetics

--- G4Simulation/TCFObase/TCFOeventAction.cxx
+++ G4Simulation/TCFObase/TCFOeventAction.cxx
@@ -180,7 +180,7 @@ void TCFOeventAction::GetHitPerPlane(const G4Event* evt, G4int idColl)
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 void TCFOeventAction::FillHits(Evento* hit, TCGmcHit* mcHit)
 {
-   G4ThreeVector vin  = mcHit->GetPosIn();
+   G4ThreeVector vin = mcHit->GetPosIn();
    G4ThreeVector vou = mcHit->GetPosOut();
    G4ThreeVector pin = mcHit->GetMomIn();
    G4ThreeVector pou = mcHit->GetMomOut();
