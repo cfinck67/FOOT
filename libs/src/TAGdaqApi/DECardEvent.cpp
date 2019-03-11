544ea7635d695de3744b7ab56031b1853c031920
Christian Finck <cfinck@iphc.cnrs.fr>
2019-03-11 15:25:13 +0100

    Fill firmware data

--- libs/src/TAGdaqApi/DECardEvent.cpp
+++ libs/src/TAGdaqApi/DECardEvent.cpp
@@ -19,30 +19,59 @@ void DECardEvent::readData(unsigned int **p1)
    // check header
    if (*p != m_vtxHeader)
       printf("Error in the event reader %x instead of %x\n", *p, m_vtxHeader);
-   values.push_back(*p);
+   
+   detectorHeader = *p;
    evtSize++;
+   values.push_back(*p);
+   
+   // 1st board header
+   boardHeader = *(++p);
+   evtSize++;
+   values.push_back(*p);
    
    // event number
    eventNumber = *(++p);
    evtSize++;
    values.push_back(*p);
+   
+   // skip 2 words and frame header
+   p += 3;
+   
+   // trigger number
+   triggerCounter = *(++p);
+   evtSize++;
+   values.push_back(*p);
+   
+   // timestamp
+   clockCounter = *(++p);
+   evtSize++;
+   values.push_back(*p);
+   
+   // frame counter
+   BCOofTrigger = *(++p);
+   evtSize++;
+   values.push_back(*p);
 
-   while (*p != m_vtxTail) {
-      values.push_back(*p);
+   do {
       p++;
       evtSize++;
-   }
-   
-   // push back trailer
-   values.push_back(*p);
+      values.push_back(*p);
+    
+   } while (*p != m_vtxTail);
 }
 
 void DECardEvent::printData () const
 {
-
-  printf ("DECardEvent DATA: \n");
-  printf ("Number of Event: %d\n",  eventNumber);
-  printf("\n");
+   printf ("DECardEvent DATA: \n");
+   printf ("Channel ID (hex): %x\n",  channelID);
+   printf ("Number of Event: %d\n",  eventNumber);
+   printf ("Data size: %d\n",  evtSize);
+   printf ("Detector Header: %x\n",  detectorHeader);
+   printf ("Board Header: %x\n",  boardHeader);
+   printf ("Trigger Counter (hw): %d\n",  triggerCounter);
+   printf ("BCO counter: %d\n",  BCOofTrigger);
+   printf ("BX counter: %d\n",  clockCounter);
+   printf("\n");
 }
 
 
