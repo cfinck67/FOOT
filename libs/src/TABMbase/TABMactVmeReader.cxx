880ad234cde50381bf75be1244a160acc5b47783
Alessio Sarti <alessio.sarti@lnf.infn.it>
2019-03-21 00:13:05 +0100

    no exec

--- libs/src/TABMbase/TABMactVmeReader.cxx
+++ libs/src/TABMbase/TABMactVmeReader.cxx
@@ -24,12 +24,12 @@ ClassImp(TABMactVmeReader);
 //! Default constructor.
 
 TABMactVmeReader::TABMactVmeReader(const char* name,
-			     TAGdataDsc* p_datraw, 
-			     TAGparaDsc* p_parmap,
-			     TAGparaDsc* p_parcon,
-			     TAGparaDsc* p_pargeo,
-			     TAGdataDsc* p_timraw, 
-              BM_struct*  p_bmstruct)
+				   TAGdataDsc* p_datraw, 
+				   TAGparaDsc* p_parmap,
+				   TAGparaDsc* p_parcon,
+				   TAGparaDsc* p_pargeo,
+				   TAGdataDsc* p_timraw, 
+				   BM_struct*  p_bmstruct)
   : TAGaction(name, "TABMactVmeReader - Unpack standalone BM raw data"),
     fpDatRaw(p_datraw),
     fpParMap(p_parmap),
