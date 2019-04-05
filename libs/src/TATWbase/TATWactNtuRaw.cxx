542decdfa703b382d769f6145226c13b09eadd76
nick <nick@Medusa.local>
2019-04-05 17:00:53 +0200

    fixed TW  geometry and global TW Foot

--- libs/src/TATWbase/TATWactNtuRaw.cxx
+++ libs/src/TATWbase/TATWactNtuRaw.cxx
@@ -111,7 +111,12 @@ Bool_t TATWactNtuRaw::Action() {
 			   Double_t rawPos=GetPosition(hita,hitb);
 			   Double_t chargeCOM=GetChargeCenterofMass(hita,hitb);
 			   //
-			   p_nturaw->NewHit(c->GetBarLayer(BarId),BarId, Energy,Time,rawPos,chargeCOM);
+			   Int_t NumberOfBarsPerLayer=NUMBEROFBARS/NUMBEROFLAYERS;
+			   // this to be consistent with the the bar id of  TATWdetector.map
+			   Int_t ShoeBarId=(BarId)%NumberOfBarsPerLayer;
+			   //
+			   std::cout << std::dec <<  "HIT " <<  channelA << " " <<channelB << " "  <<  c->GetBarLayer(BarId) << " " << BarId << " "<< ShoeBarId << " " << RawEnergy <<std::endl;
+			   p_nturaw->NewHit((int)c->GetBarLayer(BarId),ShoeBarId, Energy,Time,rawPos,chargeCOM);
 		   }
 	   }
    }
