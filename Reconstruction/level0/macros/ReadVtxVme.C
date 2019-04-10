8ae226f09eb220b0d07805f8131b9039a100cd30
Christian Finck <cfinck@iphc.cnrs.fr>
2019-04-10 11:32:01 +0200

    Add par map

--- Reconstruction/level0/macros/ReadVtxVme.C
+++ Reconstruction/level0/macros/ReadVtxVme.C
@@ -14,8 +14,10 @@
 
 #include "TAGaction.hxx"
 #include "TAGroot.hxx"
+#include "TAGgeoTrafo.hxx"
 #include "TAGactTreeWriter.hxx"
 
+#include "TAVTparMap.hxx"
 #include "TAVTparGeo.hxx"
 #include "TAVTparConf.hxx"
 #include "TAVTntuRaw.hxx"
@@ -24,7 +26,7 @@
 
 #include "TAVTactVmeReader.hxx"
 #include "TAVTactNtuClusterF.hxx"
-#include "TAVTactNtuTrackF.hxx"
+#include "TAVTactNtuTrack.hxx"
 
 #endif
 
@@ -32,10 +34,14 @@
 TAGactTreeWriter*   outFile   = 0x0;
 TAVTactVmeReader*   vmeFile   = 0x0;
 TAVTactNtuClusterF* vtActClus = 0x0;
-TAVTactNtuTrackF*   vtActTrck = 0x0;
+TAVTactNtuTrack*   vtActTrck = 0x0;
 
 void FillVertex()
 {
+   TAGparaDsc* vtMap    = new TAGparaDsc("vtMap", new TAVTparMap());
+   TAVTparMap* map   = (TAVTparMap*) vtMap->Object();
+   map->FromFile("./config/TAVTdetector.map");
+   
    
    TAGparaDsc* vtGeo    = new TAGparaDsc("vtGeo", new TAVTparGeo());
    TAVTparGeo* geomap   = (TAVTparGeo*) vtGeo->Object();
@@ -44,54 +50,56 @@ void FillVertex()
    TAGparaDsc*  vtConf  = new TAGparaDsc("vtConf", new TAVTparConf());
    TAVTparConf* parconf = (TAVTparConf*) vtConf->Object();
    parconf->FromFile("./config/TAVTdetector.cfg");
-
+   
    TAVTparConf::SetHistoMap();
    TAGdataDsc* vtNtu    = new TAGdataDsc("vtNtu", new TAVTntuRaw());
    TAGdataDsc* vtClus   = new TAGdataDsc("vtClus", new TAVTntuCluster());
    TAGdataDsc* vtTrck   = new TAGdataDsc("vtTrck", new TAVTntuTrack());
-
-
+   
+   
    vtActClus =  new TAVTactNtuClusterF("vtActClus", vtNtu, vtClus, vtConf, vtGeo);
    vtActClus->CreateHistogram();
-
-//   vtActTrck = new TAVTactNtuTrackF("vtActTrck", vtClus, vtTrck, vtConf, vtGeo);
-//   vtActTrck->CreateHistogram();
    
-//   outFile->SetupElementBranch(vtNtu, "vtrh.");
-//   outFile->SetupElementBranch(vtClus, "vtclus.");
-//   outFile->SetupElementBranch(vtTrck, "vtTrack.");
-
-   vmeFile = new TAVTactVmeReader("vmeFile", vtNtu, vtGeo, vtConf);
+   vtActTrck = new TAVTactNtuTrack("vtActTrck", vtClus, vtTrck, vtConf, vtGeo);
+   vtActTrck->CreateHistogram();
+   
+   //   outFile->SetupElementBranch(vtNtu, "vtrh.");
+   //   outFile->SetupElementBranch(vtClus, "vtclus.");
+   //   outFile->SetupElementBranch(vtTrck, "vtTrack.");
+   
+   vmeFile = new TAVTactVmeReader("vmeFile", vtNtu, vtGeo, vtConf, vtMap);
    vmeFile->CreateHistogram();
-
+   
 }
 
-void ReadVtxVme(TString path = "./run_1041", Int_t nMaxEvts = 10000)
+void ReadVtxVme(TString path = "./run_1026", Int_t nMaxEvts = 10000)
 {
-
    TAGroot tagr;
    tagr.SetCampaignNumber(100);
    tagr.SetRunNumber(1);
    
+   TAGgeoTrafo* geoTrafo = new TAGgeoTrafo();
+   geoTrafo->FromFile();
+   
    outFile = new TAGactTreeWriter("outFile");
-
+   
    FillVertex();
    vmeFile->Open(path);
    
    tagr.AddRequiredItem(vmeFile);
    tagr.AddRequiredItem(vtActClus);
-//   tagr.AddRequiredItem(vtActTrck);
+   // tagr.AddRequiredItem(vtActTrck);
    tagr.AddRequiredItem(outFile);
-
+   
    tagr.Print();
    
    TString outFileName = path;
    outFileName.Append(".root");
    if (outFile->Open(outFileName.Data(), "RECREATE")) return;
    vmeFile->SetHistogramDir(outFile->File());
-  vtActClus->SetHistogramDir(outFile->File());
-//   vtActTrck->SetHistogramDir(outFile->File());
-
+   vtActClus->SetHistogramDir(outFile->File());
+   vtActTrck->SetHistogramDir(outFile->File());
+   
    cout<<" Beginning the Event Loop "<<endl;
    tagr.BeginEventLoop();
    TStopwatch watch;
@@ -100,12 +108,12 @@ void ReadVtxVme(TString path = "./run_1041", Int_t nMaxEvts = 10000)
    Int_t nEvents = 0;
    while (tagr.NextEvent() ){
       
-     // printf("\n");
+      // printf("\n");
       if (++nEvents % 100 == 0)
-		printf("Event: %d\n", nEvents); 
-	  
-	  if (nEvents == nMaxEvts)
-		 break;
+         printf("Event: %d\n", nEvents);
+      
+      if (nEvents == nMaxEvts)
+         break;
    }
    
    tagr.EndEventLoop();
@@ -114,7 +122,7 @@ void ReadVtxVme(TString path = "./run_1041", Int_t nMaxEvts = 10000)
    outFile->Close();
    
    watch.Print();
-
+   
 }
 
 
