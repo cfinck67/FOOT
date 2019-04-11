482c3091336c4563ed277290af0e2e917ccc573c
Christian Finck <cfinck@iphc.cnrs.fr>
2019-04-11 09:47:45 +0200

    Remove unused classes

--- libs/src/TATWbase/TATWbase.LinkDef.h
+++ libs/src/TATWbase/TATWbase.LinkDef.h
@@ -19,10 +19,6 @@
 #pragma link C++ class TATWactNtuMC+;
 #pragma link C++ class TATWactNtuPoint+;
 #pragma link C++ class TATWparGeo+;
-#pragma link C++ class TATW_ContainerHit+;
-#pragma link C++ class TATW_ContainerPoint+;
-#pragma link C++ class TATW_Hit+;
-#pragma link C++ class TATW_Point+;
 #pragma link C++ class TATWdigitizer+;
 
 #endif
