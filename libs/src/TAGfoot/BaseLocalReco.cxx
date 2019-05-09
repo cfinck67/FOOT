4a03b90ec8657b5d73f4e85f67ac01692a92a1b2
Christian Finck <cfinck@iphc.cnrs.fr>
2019-05-09 08:52:38 +0200

    Add reading parameters file

--- libs/src/TAGfoot/BaseLocalReco.cxx
+++ libs/src/TAGfoot/BaseLocalReco.cxx
@@ -93,6 +93,7 @@ BaseLocalReco::~BaseLocalReco()
 //__________________________________________________________
 void BaseLocalReco::BeforeEventLoop()
 {
+   ReadParFiles();
    CreateRawAction();
    CreateRecAction();
 
